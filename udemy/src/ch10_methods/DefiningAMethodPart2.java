5e9d2dead70442e6aebba6b405e15f1746a1d2cc
Raul Zuniga <rzuniga64@gmail.com>
2013-12-18 12:06:49 -0600

    Add udemy java method programming files

---
+++ udemy/src/ch10_methods/DefiningAMethodPart2.java
@@ -0,0 +1,23 @@
+package ch10_methods;
+
+import java.util.Scanner;
+
+public class DefiningAMethodPart2
+{
+   public static void main(String[] arts) 
+   {
+      
+   }
+   
+   static double ftoc(double temp) {
+      double celsius;
+      celsius = (temp - 32.0) * (5.0 / 9.0);
+      return celsius;
+   }
+
+   static double ctof(double temp) {
+      double fahrenheit;
+      fahrenheit = temp * (9.0 / 5.0) + 32.0;
+      return fahrenheit;
+   }
+}
