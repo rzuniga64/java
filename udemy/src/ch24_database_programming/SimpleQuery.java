5fe457aa2188891219d1f12cb2482c3b868323ea
Raul Zuniga <rzuniga64@gmail.com>
2013-12-17 18:30:30 -0600

    Add udemy java datgabase programming files

---
+++ udemy/src/ch24_database_programming/SimpleQuery.java
@@ -0,0 +1,29 @@
+import java.sql.*;
+
+public class SimpleQuery {
+   public static void main(String[] args) {
+      Connection conn = null;
+      try {
+         String userName = "root";
+         String password = "aggies92";
+         String url = "jdbc:mysql://localhost:3306/sakila?zeroDateTimeBehavior=convertToNull";
+         Class.forName("com.mysql.jdbc.Driver").newInstance();
+         conn = DriverManager.getConnection(url, userName, password);
+         Statement stmt = conn.createStatement();
+         ResultSet rset = stmt.executeQuery("select * from actor");
+         while (rset.next()) {
+            int actorID = rset.getInt("actor_ID");
+            String fname = rset.getString("first_name");
+            String lname = rset.getString("last_name");            
+            System.out.print("Actor ID: " + actorID + " ");
+            System.out.print("First Name: " + fname + "\t");
+            System.out.println("Last Name " + lname);
+         }
+         rset.close();
+         conn.close();
+      }
+      catch (Exception e) {
+         System.out.print("Error: " + e.toString());
+      }
+   }
+}
---
+++ udemy/src/ch24_database_programming/TestDriver.java
@@ -0,0 +1,14 @@
+package ch24_database_programming;
+
+public class TestDriver {
+   public static void main(String[] args) {
+      try {
+        // This will load the MySQL server, each DB has its own server
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+        System.out.println("Database connection successful.");
+      }
+      catch (Exception e) {
+        System.out.println("Cannot connect to database.");
+      }
+   }
+}
