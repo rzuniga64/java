3d6195a48d45a4f3b26d705dc55d2ac1ea389cb3
Raul Zuniga <rzuniga64@gmail.com>
2013-12-20 23:12:25 -0600

    Add udemy arraylist examples

---
+++ udemy/src/ch11_arraylist/ArrayListIntro.java
@@ -0,0 +1,20 @@
+package ch11_arraylist;
+
+import java.util.ArrayList;
+
+public class ArrayListIntro
+{
+   public static void main(String[] args)
+   {
+      double average;
+      int total = 0;
+      ArrayList<Integer> grades = new ArrayList<Integer>();
+      grades.add(78);
+      grades.add(84);
+      grades.add(90);
+      for(int grade : grades)
+         total += grade;
+      average = total / grades.size();
+      System.out.println("The average is " + average);
+   }
+}      
