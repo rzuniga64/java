07521587c044f085a27f4533fa7c9f15a4714f4e
Raul Zuniga <rzuniga64@gmail.com>
2015-07-03 02:43:51 -0500

    commit data structure files

---
+++ udemy/src/advanced_java/ch5_data_structures/NameQueue.java
@@ -0,0 +1,34 @@
+package advanced_java.ch5_data_structures;
+
+import java.util.LinkedList;
+
+public class NameQueue {
+    public static void main(String[] args) {
+        LinkedList<String> names = new LinkedList<>();
+        names.addLast("Cynthia");
+        names.addLast("Raymond");
+        names.addLast("David");
+        for (String name : names)
+            System.out.println(name);
+        System.out.println();
+
+        names.removeFirst();
+        for (String name : names)
+            System.out.println(name);
+        System.out.println();
+
+        names.addLast("Clayton");
+        for (String name : names)
+            System.out.println(name);
+        System.out.println();
+        System.out.println("Size of queue: " + names.size());
+        if (!names.isEmpty()) {
+            names.removeFirst();
+        }
+        System.out.println("Size of queue: " + names.size());
+
+        System.out.println();
+        for (String name : names)
+            System.out.println(name);
+    }
+}
