544e313ddc7a34b51376db7a60e79e6b975d599a
Raul Zuniga <rzuniga64@gmail.com>
2015-07-04 22:52:59 -0500

    commit sort and search  files

---
+++ udemy/src/advanced_java/ch6_sort_and_search/BubbleSort.java
@@ -0,0 +1,37 @@
+package advanced_java.ch6_sort_and_search;
+
+/**
+ * Created by Roy on 7/3/2015.
+ */
+public class BubbleSort {
+
+    public static void bubbleSort(int[] data) {
+        int size = data.length;
+        for (int i = 1; i < size; i++) {
+            for (int j = 0; j <  size - i; j++) {
+                if (data[j] > data[j+1]) {
+                    int temp = data[j];
+                    data[j] = data[j+1];
+                    data[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int size = 10;
+        int[] data = new int[size];
+        for (int i = 0; i < size; i++)
+            data[i] = (int) (Math.random() * 100);
+
+        System.out.println("Before sorting: ");
+        for (int i = 0; i < size; i++)
+            System.out.println(data[i]);
+
+        bubbleSort(data);
+
+        System.out.println("\nAfter sorting: ");
+        for (int i = 0; i < size; i++)
+            System.out.println(data[i]);
+    }
+}
