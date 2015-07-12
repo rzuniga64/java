80bdc3bf9fff4f82fd4b5154fd0681198f15a9e9
Raul Zuniga <rzuniga64@gmail.com>
2015-07-12 17:04:48 -0500

    commit simple servlet  files

---
+++ udemy/src/advanced_java/ch13_simple_servlet/HelloWorldServlet.java
@@ -0,0 +1,23 @@
+package advanced_java.ch13_simple_servlet;
+/*
+    I had to copy this file and compile it in directory
+    C:\xampp\tomcat\webapps\examples\WEB-INF\classes.
+    Then I had to modify C:\xampp\tomcat\webapps\examples\WEB-INF\web.xml
+    and add servlet and servlet-mapping tags.
+    Then the server must be started and in the browser type
+    localhost:8080/examples/servlets/servlet/HelloWorldServlet.
+ */
+
+import java.io.*;
+import javax.servlet.*;
+
+public class HelloWorldServlet extends GenericServlet {
+   public void service(ServletRequest request, ServletResponse response) 
+                       throws ServletException, IOException {
+      response.setContentType("text/html");
+      PrintWriter pw = response.getWriter();
+      pw.println("Hello, world!");
+      pw.close();
+   }
+}
+      
---
+++ udemy/src/advanced_java/ch13_simple_servlet/PostSelServlet.java
@@ -0,0 +1,17 @@
+package advanced_java.ch13_simple_servlet;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class PostSelServlet extends HttpServlet {
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response)
+                       throws IOException, ServletException {
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        String language = request.getParameter("lang");
+        writer.println("You selected " + language + " as your favorite.");
+        writer.close();
+    }
+}
