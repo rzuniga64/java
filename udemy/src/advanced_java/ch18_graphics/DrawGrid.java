84a722ad4367b35158d70076e7e23b574a480d69
Raul Zuniga <rzuniga64@gmail.com>
2015-07-14 17:50:24 -0500

    commit graphics  files

---
+++ udemy/src/advanced_java/ch18_graphics/DrawGrid.java
@@ -0,0 +1,23 @@
+package advanced_java.ch18_graphics;
+
+import java.awt.*;
+import java.applet.*;
+
+//<applet code="DrawGrid" width=500 height=500></applet>
+
+public class DrawGrid extends Applet {
+    static void grid(Graphics g, int side) {
+        for (int x = 0; x < side; x++)
+            for (int y = 0; y < side; y++)
+                g.drawRect(x*side, y*side, side, side);
+    }
+
+    public void paint(Graphics g) {
+        int s = 20;
+        grid(g,s);
+    }
+
+
+}
+
+
