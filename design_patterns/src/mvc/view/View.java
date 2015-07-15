b82db0e6c5c33a5481411903d1c1fbd1ef014792
Raul Zuniga <rzuniga64@gmail.com>
2015-07-15 18:07:56 -0500

    add design pattern files

---
+++ design_patterns/src/mvc/view/View.java
@@ -0,0 +1,19 @@
+package mvc.view;
+
+import javax.swing.JFrame;
+
+import mvc.model.Model;
+
+public class View extends JFrame {
+	
+	private Model model;
+
+	public View(Model model) {
+		super("MVC Demo");
+		
+		this.model = model;
+	}
+	
+	
+
+}
