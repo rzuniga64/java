2d36f77e8b567b1bfa1497f353c205a58f3a1f50
Raul Zuniga <rzuniga64@gmail.com>
2015-07-16 20:00:51 -0500

    add low level thread synchronization  files

---
+++ multithreading/src/lesson9_lowlevel_thread_synchronization/App.java
@@ -0,0 +1,43 @@
+package lesson9_lowlevel_thread_synchronization;
+
+/**
+ * How to implement the Producer-Consumer pattern using "low level" techniques;
+ * namely, wait, notify and synchronized. This isn't the best way to implement a
+ * Producer-Consumer pattern in Java
+ * (see tutorial 7 use of {@link java.util.concurrent.BlockingQueue} for
+ * the best way); but this tutorial will help you to understand how to use wait
+ * and notify.
+ */
+public class App {
+
+    public static void main(String[] args) throws InterruptedException {
+        final Processor processor = new Processor();
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    processor.produce();
+                } catch (InterruptedException ignored) {}
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    processor.consume();
+                } catch (InterruptedException ignored) {}
+            }
+        });
+
+        t1.start();
+        t2.start();
+//        t1.join();
+//        t2.join();
+
+        // Pause for 30 seconds and force quitting the app (because we're
+        // looping infinitely)
+        Thread.sleep(30000);
+        System.exit(0);
+    }
+}
---
+++ multithreading/src/lesson9_lowlevel_thread_synchronization/Processor.java
@@ -0,0 +1,61 @@
+package lesson9_lowlevel_thread_synchronization;
+
+import java.util.LinkedList;
+import java.util.Random;
+
+/**
+ * It's better to use a pre-built class like ArrayBlockingQueue and not go low-level.
+ * Then you can be pretty sure that even multiple consumers and multiple producers will
+ * work just fine.
+ */
+@SuppressWarnings("InfiniteLoopStatement")
+public class Processor {
+
+    private LinkedList<Integer> list = new LinkedList<>();
+    private final int LIMIT = 10;
+
+    // used to call wait and notify on the object you are locking on
+    private final Object lock = new Object();
+
+    public void produce() throws InterruptedException {
+        int value = 0;
+        Random random = new Random();
+        while (true) {
+            synchronized (lock) {
+                //whenever the thread is notified starts again from the loop
+                while (list.size() == LIMIT)
+                    lock.wait();// wait() is also true
+
+                list.add(value);
+
+                System.out.println("Producer added:   " + value + " queue size is: " + list.size());
+                value++;
+                lock.notify();
+                Thread.sleep(random.nextInt(1000)); //force producer fill the queue to LIMIT_SIZE
+            }
+        }
+    }
+
+    /**
+     * The consume method can acquire the lock whenever the produce method calls notify() on the lock.
+     * This happens each time a value is added to the list - regardless of whether or not list.size()
+     * LIMIT has been reached. In "startup mode" - i.e. while the list is initially being filled -
+     * the lock is up for grabs by either thread, so the first reported size will not always be the same.
+     * @throws InterruptedException
+     */
+    public void consume() throws InterruptedException {
+        Random random = new Random();
+        while (true) {
+            synchronized (lock) {
+                while (list.size() == 0)
+                    lock.wait();
+
+                int value = list.removeFirst();
+                System.out.print("Consumer removed: " + value);
+                System.out.println(" queue size is: " + list.size());
+                lock.notify();
+            }
+            //Thread.sleep(random.nextInt(100)); //force producer fill the queue to LIMIT_SIZE
+        }
+    }
+}
