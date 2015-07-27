c4517285dbf0f50db1fb9f481ae83cecb8676f51
Raul Zuniga <rzuniga64@gmail.com>
2015-07-27 03:50:00 -0500

    commiting Hibernate files

--- hibernate/src/main/java/com/infiniteskills/data/applications/_07_hibernate_api/ApplicationFlushPersistenceContext.java
+++ hibernate/src/main/java/com/infiniteskills/data/applications/_07_hibernate_api/ApplicationFlushPersistenceContext.java
@@ -8,43 +8,44 @@ import com.infiniteskills.data.entities._07_hibernate_api.*;
 import com.infiniteskills.data.utilities.HibernateUtil;
 import org.hibernate.Session;
 
-public class ApplicationSaveOrUpdate {
+public class ApplicationFlushPersistenceContext {
 
 	public static void main(String[] args) {
+		// Open Persistence Context 1
+		Session session = HibernateUtil.getSessionFactory().openSession();
+        // When we want to write changes to the database we need a connection. A transaction houses that
+        // connection. With a transaction we want to perform all the work as one atomic unit.  If one of the operations
+        // fails we need to rollback everything. That's the whole point of a transaction. So every time we are working
+        // with a session we are working with a transaction.
+		org.hibernate.Transaction transaction = session.beginTransaction();
 		try {
-			// Open Persistence Context 1
-			Session session = HibernateUtil.getSessionFactory().openSession();
-			org.hibernate.Transaction transaction = session.beginTransaction();
-			// Put the entity in the persistence context
-			BankHibernateAPI detachedBank = (BankHibernateAPI) session.get(BankHibernateAPI.class, 1L);
-			// Persist the entity to the database
+			// Put the entity in the persistence context which keeps track of all changes to all entities.
+			BankHibernateAPI bank = (BankHibernateAPI) session.get(BankHibernateAPI.class, 1L);
+			bank.setName("Something Different");
+			System.out.println("Calling Flush");
+
+            // When flush is called we want Hibernate to take all the changes within our persistence context and synch
+            // them with the database.
+			session.flush();
+
+            // Make a second change to the entity after calling flush.
+			bank.setAddressLine1("Another Address Line");
+			System.out.println("Calling commit");
+			// Persist the entity to the database. When we work with a transaction one of the ways we cause
+            // all of the changes to be made on the database is to call the commit method on the transaction.
+            // This commit is forcing a flush on the session. There are three times when a flush will be called on a
+            // session:
+            // 1. Call session.flush()
+            // 2. When a commit is called on a transaction.
+            // 3. Sometimes when we run a SELECT operation a flush will occur before it.
 			transaction.commit();
-			// Close Persistence Context 1. This bank entity's state is detached.
-			session.close();
-
-            // Create a new bank entity using new operator and constructor.
-            // This bank entity's state is transient.
-			BankHibernateAPI transientBank = createBank();
-
-            // Open Persistence Context 2
-			Session session2 = HibernateUtil.getSessionFactory().openSession();
-			org.hibernate.Transaction transaction2 = session2.beginTransaction();
-
-            // saveOrUpdate is flexible method that handles both detached and transient entity states.
-            // Hibernate looks at the identifier property for the entity and using the presence or lack
-            // of identifier the API is smart enough to route the method invocation to the appropriate
-            // method saveOrUpdate.
-			session2.saveOrUpdate(transientBank);
-			session2.saveOrUpdate(detachedBank);
-			detachedBank.setName("Test Bank 2");
-            // Persist the entity to the database
-			transaction2.commit();
-            // Close Persistence Context 2.
-			session2.close();
-			
 		} catch (Exception e) {
+            // Make sure that if that there are any exceptions then rollback the transaction.
+			transaction.rollback();
 			e.printStackTrace();
 		}finally{
+			// Close Persistence Context.
+			session.close();
 			HibernateUtil.getSessionFactory().close();
 		}
 	}
