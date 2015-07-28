d3cb109233c72792e4d82e341c9238788b881474
Raul Zuniga <rzuniga64@gmail.com>
2015-07-27 19:25:28 -0500

    Add oop review files

---
+++ design_patterns/src/oop_review/Dog.java
@@ -0,0 +1,36 @@
+package oop_review;
+
+public class Dog extends Animal{
+
+    // The constructor initializes all objects
+    public Dog(){
+
+        // Executes the parents constructor
+        // Every class has a constructor whether you make it or not
+        super();
+
+        // Sets bark for all Dog objects by default
+        setSound("Bark");
+    }
+
+    public void digHole(){
+
+        System.out.println("Dug a hole");
+    }
+
+    public void changeVar(int randNum){
+
+        randNum = 12;
+        System.out.println("randNum in method value: " + randNum);
+    }
+
+	/* This private method can only be accessed through using other
+	 * methods in the class */
+    private void bePrivate(){
+        System.out.println("In a private method");
+    }
+
+    public void accessPrivate(){
+        bePrivate();
+    }
+}
---
+++ design_patterns/src/oop_review/Giraffe.java
@@ -0,0 +1,72 @@
+package oop_review;
+
+public class Giraffe extends Creature{
+
+    private String name;
+
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        return name;
+    }
+
+    @Override
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    @Override
+    public double getHeight() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void setHeight(double newHeight) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public double getWeight() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void setWeight(double newWeight) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public String getFavFood() {
+        // TODO Auto-generated method stub
+        return favFood;
+    }
+
+    @Override
+    public void setFavFood(String newFood) {
+        name = newFood;
+    }
+
+    @Override
+    public double getSpeed() {
+        // TODO Auto-generated method stub
+        return speed;
+    }
+
+    @Override
+    public void setSpeed(double newSpeed) {
+        speed = newSpeed;
+    }
+
+    @Override
+    public String getSound() {
+        // TODO Auto-generated method stub
+        return sound;
+    }
+
+    @Override
+    public void setSound(String newSound) {
+        sound = newSound;
+    }
+}
