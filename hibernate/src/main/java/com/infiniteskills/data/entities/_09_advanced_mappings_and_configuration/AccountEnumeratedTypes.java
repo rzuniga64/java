898b67181b6bf20c289f78872b7cdd443aa37d69
Raul Zuniga <rzuniga64@gmail.com>
2015-07-31 19:40:45 -0500

    commiting Hibernate files

--- hibernate/src/main/java/com/infiniteskills/data/entities/_09_advanced_mappings_and_configuration/AccountEnumeratedTypes.java
+++ hibernate/src/main/java/com/infiniteskills/data/entities/_09_advanced_mappings_and_configuration/AccountEnumeratedTypes.java
@@ -11,23 +11,20 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "ACCOUNT")
+// @NamedQueries is a JPA annotation and it accepts an array of named queries.  The name must be unique for the
+// persistence context.
+@NamedQueries({
+        @NamedQuery(name="Account.largeDeposits", query= "select distinct t.account from TransactionEnumeratedTypes t"
+				+ " where t.amount > 500 and lower(t.transactionType) = 'deposit'"),
+        @NamedQuery(name="Account.byWithdrawlAmount", query= "select distinct t.account.name, "
+                + "concat(concat(t.account.bank.name, ' '),t.account.bank.addressHibernateAPI.state)"
+                + " from TransactionEnumeratedTypes t"
+                + " where t.amount > :amount and t.transactionType = 'Withdrawl'")
+})
 public class AccountEnumeratedTypes {
 
 	@Id
