46a13fe2f8367959d691aefcd48a832d8b3aa8df
Raul Zuniga <rzuniga64@gmail.com>
2015-08-01 01:48:08 -0500

    commiting Hibernate files

---
+++ hibernate/src/main/java/com/infiniteskills/data/applications/_11_criteria_api/AppHibernateRestrictions.java
@@ -0,0 +1,152 @@
+package com.infiniteskills.data.applications._11_criteria_api;
+
+import com.infiniteskills.data.entities._07_hibernate_api.*;
+import com.infiniteskills.data.utilities.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+
+public class AppHibernateRestrictions {
+
+	public static void main(String[] args) {		
+		SessionFactory sessionFactory = null;
+		Session session = null;
+		org.hibernate.Transaction tx = null;
+
+		try {
+			sessionFactory = HibernateUtil.getSessionFactory();
+			session = sessionFactory.openSession();
+			tx = session.beginTransaction();
+
+			Criterion criterion1 = Restrictions.le("amount", new BigDecimal("20.00"));
+			Criterion criterion2 = Restrictions.eq("transactionType", "Withdrawl");
+
+			/*List<TransactionHibernateAPI> transactions = session
+					.createCriteria(TransactionHibernateAPI.class).add(criterion1).add(criterion2)
+					.addOrder(Order.desc("title")).list();*/
+
+            List<TransactionHibernateAPI> transactions = session
+                    .createCriteria(TransactionHibernateAPI.class).add(Restrictions.and(criterion1,criterion2))
+                    .addOrder(Order.desc("title")).list();
+
+			for (TransactionHibernateAPI transaction: transactions)
+				System.out.println(transaction.getTitle());
+			
+			tx.commit();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			tx.rollback();
+		} finally {
+			session.close();
+			sessionFactory.close();
+		}
+	}
+	
+	protected static BankHibernateAPI createBank() {
+		BankHibernateAPI bank = new BankHibernateAPI();
+		bank.setName("First United Federal");
+		bank.setAddressLine1("103 Washington Plaza");
+		bank.setAddressLine2("Suite 332");
+		bank.setAddressType("PRIMARY");
+		bank.setCity("New York");
+		bank.setCreatedBy("Kevin Bowersox");
+		bank.setCreatedDate(new Date());
+		bank.setInternational(false);
+		bank.setLastUpdatedBy("Kevin Bowersox");
+		bank.setLastUpdatedDate(new Date());
+		bank.setState("NY");
+		bank.setZipCode("10000");
+		return bank;
+	}
+
+	protected static UserHibernateAPI createUser() {
+		UserHibernateAPI user = new UserHibernateAPI();
+		AddressHibernateAPI address = createAddress();
+		user.setAddresses(Arrays.asList(new AddressHibernateAPI[] { createAddress() }));
+		user.setBirthDate(new Date());
+		user.setCreatedBy("Kevin Bowersox");
+		user.setCreatedDate(new Date());
+		user.setCredential(createCredential(user));
+		user.setEmailAddress("test@test.com");
+		user.setFirstName("John");
+		user.setLastName("Doe");
+		user.setLastUpdatedBy("system");
+		user.setLastUpdatedDate(new Date());
+		return user;
+	}
+
+	protected static CredentialHibernateAPI createCredential(UserHibernateAPI user) {
+		CredentialHibernateAPI credential = new CredentialHibernateAPI();
+		credential.setUser(user);
+		credential.setUsername("test_username");
+		credential.setPassword("test_password");
+		return credential;
+	}
+
+	protected static AddressHibernateAPI createAddress() {
+		AddressHibernateAPI address = new AddressHibernateAPI();
+		address.setAddressLine1("101 Address Line");
+		address.setAddressLine2("102 Address Line");
+		address.setCity("New York");
+		address.setState("PA");
+		address.setZipCode("10000");
+		address.setAddressType("PRIMARY");
+		return address;
+	}
+
+	protected static TransactionHibernateAPI createNewBeltPurchase(AccountHibernateAPI account) {
+		TransactionHibernateAPI beltPurchase = new TransactionHibernateAPI();
+		beltPurchase.setAccount(account);
+		beltPurchase.setTitle("Dress Belt");
+		beltPurchase.setAmount(new BigDecimal("50.00"));
+		beltPurchase.setClosingBalance(new BigDecimal("0.00"));
+		beltPurchase.setCreatedBy("Kevin Bowersox");
+		beltPurchase.setCreatedDate(new Date());
+		beltPurchase.setInitialBalance(new BigDecimal("0.00"));
+		beltPurchase.setLastUpdatedBy("Kevin Bowersox");
+		beltPurchase.setLastUpdatedDate(new Date());
+		beltPurchase.setNotes("New Dress Belt");
+		beltPurchase.setTransactionType("Debit");
+		return beltPurchase;
+	}
+
+	protected static TransactionHibernateAPI createShoePurchase(AccountHibernateAPI account) {
+		TransactionHibernateAPI shoePurchase = new TransactionHibernateAPI();
+		shoePurchase.setAccount(account);
+		shoePurchase.setTitle("Work Shoes");
+		shoePurchase.setAmount(new BigDecimal("100.00"));
+		shoePurchase.setClosingBalance(new BigDecimal("0.00"));
+		shoePurchase.setCreatedBy("Kevin Bowersox");
+		shoePurchase.setCreatedDate(new Date());
+		shoePurchase.setInitialBalance(new BigDecimal("0.00"));
+		shoePurchase.setLastUpdatedBy("Kevin Bowersox");
+		shoePurchase.setLastUpdatedDate(new Date());
+		shoePurchase.setNotes("Nice Pair of Shoes");
+		shoePurchase.setTransactionType("Debit");
+		return shoePurchase;
+	}
+
+	protected static AccountHibernateAPI createNewAccount() {
+		AccountHibernateAPI account = new AccountHibernateAPI();
+		account.setCloseDate(new Date());
+		account.setOpenDate(new Date());
+		account.setCreatedBy("Kevin Bowersox");
+		account.setInitialBalance(new BigDecimal("50.00"));
+		account.setName("Savings Account");
+		account.setCurrentBalance(new BigDecimal("100.00"));
+		account.setLastUpdatedBy("Kevin Bowersox");
+		account.setLastUpdatedDate(new Date());
+		account.setCreatedDate(new Date());
+		return account;
+	}
+
+}
