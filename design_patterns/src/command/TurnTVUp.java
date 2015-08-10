2020ccf85ae19a1d8751324a0d20955bf0ce3dd5
Raul Zuniga <rzuniga64@gmail.com>
2015-08-10 00:26:46 -0500

    Add abstract factory  files

---
+++ design_patterns/src/command/TurnTVUp.java
@@ -0,0 +1,18 @@
+package command;
+
+public class TurnTVUp implements Command {
+
+    ElectronicDevice theDevice;
+
+    public TurnTVUp(ElectronicDevice newDevice){
+        theDevice = newDevice;
+    }
+
+    public void execute() {
+        theDevice.volumeUp();
+    }
+
+    public void undo() {
+        theDevice.volumenDown();
+    }
+}
