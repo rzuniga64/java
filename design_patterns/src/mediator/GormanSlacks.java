ac6c775aa1907bf2e1bd9411649c7528ddd7bdc8
Raul Zuniga <rzuniga64@gmail.com>
2015-08-19 01:18:20 -0500

    Add chain of responsibility design pattern  files

---
+++ design_patterns/src/mediator/GormanSlacks.java
@@ -0,0 +1,10 @@
+package mediator;
+
+public class GormanSlacks extends Colleague{
+
+    public GormanSlacks(Mediator newMediator) {
+        super(newMediator);
+
+        System.out.println("Gorman Slacks signed up with the stockexchange\n");
+    }
+}
