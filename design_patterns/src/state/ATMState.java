b3d56a42b87d36d3f4f680d490fb68761507ddff
Raul Zuniga <rzuniga64@gmail.com>
2015-08-25 10:38:50 -0500

    Update design patterns

---
+++ design_patterns/src/state/ATMState.java
@@ -0,0 +1,12 @@
+package state;
+
+public interface ATMState {
+
+    // Different states expected
+    // HasCard, NoCard, HasPin, NoCash
+
+    void insertCard();
+    void ejectCard();
+    void insertPin(int pinEntered);
+    void requestCash(int cashToWithdraw);
+}
