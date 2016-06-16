493a0b7c33d607caff54e19690868e35be6e0519
Raul Zuniga <rzuniga64@gmail.com>
2016-06-16 16:19:17 -0500

    Update algorithms directory

--- algorithms/src/recursion/Recursion.java
+++ algorithms/src/recursion/Recursion.java
@@ -1,5 +1,6 @@
 package recursion;
 
+import data_structure.linkedlist.LinkedList;
 
 /**
  * WHY USE RECURSION?
@@ -55,6 +56,29 @@ public class Recursion {
         System.out.println("GCD(9.1): " + gcd(9,2) + "\n");
         System.out.println("GCD(9.1): " + gcd(70,25) + "\n");
         System.out.println("GCD(9.1): " + gcd(25,70) + "\n");
+
+        // Table of squares
+        tableOfSquares(10);
+
+        // Power of a number
+        System.out.println("\n" + 2 + " to the power of " + 2 + " is " + power(2,2));
+        System.out.println(3 + " to the power of " + 3 + " is " + power(3,3));
+
+        // recursion over a list
+        int[] list = { 1, 2, 3, 4, 5, 6, 7, 9, 10, 11 };
+        System.out.println("\nIs " + number + " a member of array? " + isMember(list, 9, 11));
+
+        // Maximum value of a linked list
+        LinkedList theLinkedList = new LinkedList();
+        theLinkedList.insert(12.0);
+        theLinkedList.insert(24.0);
+        theLinkedList.insert(100.0);
+        theLinkedList.insert(90.0);
+
+        LinkedList.Link link = theLinkedList.getFirstLink();
+        System.out.println("\nThe max value of the list is " + maxValue(link, 1.00));
+
+
     }
 
     /**
@@ -68,7 +92,7 @@ public class Recursion {
     *   It�s a recursive definition
     */
 
-    static int fib(int x) {
+    private static int fib(int x) {
         if (x <= 1)
             return x;
         else
@@ -87,7 +111,7 @@ public class Recursion {
      * n! = n * (n-1)!
      */
 
-    static int factorial( int n) {
+    private static int factorial( int n) {
         if (n == 0)
             return 1;
         else
@@ -95,7 +119,6 @@ public class Recursion {
     }
 
     /**
-     * Recursive function example
      * greatest common divisor
      * Greatest common divisor of two non-zero ints is the largest positive integer
      * that divides the numbers without a remainder.
@@ -111,12 +134,64 @@ public class Recursion {
      *  This moves the larger number to the first position.
      */
 
-    static int gcd(int x, int y) {
+    private static int gcd(int x, int y) {
         System.out.println("gcd called with " + x + " and " + y);
         if (x % y == 0)
             return y;
         else {
             return gcd(y, x % y);
         }
+    }
+
+    /**
+     * Table of squares
+     * Precondiction: none
+     * Postcondition: return a table of squares from 1 to n
+     */
+
+    private static void tableOfSquares(int number){
+        if (number > 0) {
+            tableOfSquares(number - 1);
+            System.out.printf( "%2d %3d \n", number, number*number);
+        }
+    }
+
+    /**
+     * Power of a number
+     * Preconditions: takes two arguments, the number raised to the power and the power
+     * PostConditions: returns a number raised to a power using recursion
+     */
+
+    private static double power(double num, int pow) {
+        if (pow == 0)
+            return 1;
+        else
+            return num * power(num, pow - 1);
+    }
+
+    /**
+     *  Member of an aray
+     *  Preconditions: the function takes three arguments: an array of ints, its size and a target value.
+     *  Postconditions: returns true if the target value is found in the array, or false if the value is not in the array
+     */
+
+    private static boolean isMember(int array[], int size, int value) {
+        return (array[size] == value || isMember(array, size-1, value));
+    }
+
+    /**
+     * Maximum value of an array
+     * Precondition: The function takes two argument: a Link, and the value to compare against.
+     * PostCondition: returns the maximum value in a linked list
+     */
+
+    private static Double maxValue(LinkedList.Link Link, Double maxValue) {
+        if (Link == null)
+            return maxValue;
+        if ( (Double)Link.getValue() > maxValue)
+            return maxValue(Link.getNext(), (Double) Link.getValue());
+        else
+            return maxValue(Link.getNext(), maxValue);
+
     }
 }
