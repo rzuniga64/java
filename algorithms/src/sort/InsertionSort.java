85d96e393555e2aa135fbdc8d8421326a5303c1f
Raul Zuniga <rzuniga64@gmail.com>
2016-06-20 14:39:47 -0500

    Update algorithms directory

--- algorithms/src/sort/InsertionSort.java
+++ algorithms/src/sort/InsertionSort.java
@@ -29,7 +29,6 @@ public class InsertionSort {
         System.out.println();
 
         // END OF FIRST PART
-
         // ADDED FOR BUBBLE SORT
         if(j != -1){
             // ADD THE +2 TO FIX SPACING
@@ -75,24 +74,50 @@ public class InsertionSort {
     }
 
     /**
-     *  The Insertion sort.SelectionSort is normally the best of the elementary sorts. Unlike the other sorts that had a group sorted
-     *  at any given time, groups are only partially sorted here.
-     *  */
+     *  The Insertion sort.SelectionSort is normally the best of the elementary sorts.
+     *  Unlike the other sorts that had a group sorted at any given time, groups are only partially sorted here.
+     *
+     * Insertion sort is a simple sorting algorithm that is appropriate for small inputs
+     * The basic action of insertion sort is to sort the elements in positions 0 through p (where p ranges from 1 through
+     * N-1). In each state p increases by 1. That is what the outer loop is controlling.
+     *
+     * When the body of the for loop is entered we are guaranteed that the elements in array positions 0 through p-1 have
+     * already been sorted and that we need to extend this to positions 0 to p.
+     *
+     * At each step the underlined element needs to be added to the previously sorted part of the array. We can easily
+     * do that by placing it in a temporary variable and sliding all the elements that ARE LARGER THAN IT on position
+     * to the right. We then copy the temporary variable into the former position of the leftmost relocated
+     * element. We keep a counter j, which is the position in which the temporary variable should be written back.
+     * Every time an element is slid, j decreases by 1.
+     *
+     * Array Position           0   1   2   3   4   5
+     * ----------------------------------------------
+     * Initial state            8|  5   9   2   6   3   temp = 5
+     *                              -
+     * After a[0..1] is sorted  5   8|  9   2   6   3   temp = 9
+     *                                  -
+     * After a[0..2] is sorted  5   8   9|  2   6   3   temp = 2
+     *                                      -
+     * After a[0..3] is sorted  2   5   8   9|  6   3   temp = 6
+     *                                          -
+     * After a[0..4] is sorted  2   5   6   8   9|  3   temp = 3
+     *                                              -
+     * After a[0..5] is sorted  2   3   5   6   8   9|
+     **/
 
     private void insertionSort(){
 
         for (int i = 1; i < arraySize; i++){
-            int j = i;
-            int toInsert = theArray[i];
-            while ((j > 0) && (theArray[j-1] > toInsert)){
+            int temp = theArray[i];
+            int j;
+            for (j = i; j > 0 && theArray[j-1] > temp; j-- ){
                 theArray[j] = theArray[j-1];
-                j--;
                 printHorizontalArray(i, j);
             }
 
-            theArray[j] = toInsert;
+            theArray[j] = temp;
             printHorizontalArray(i, j);
-            System.out.println("\nArray[i] = " + theArray[i] + " Array[j] = " + theArray[j] + " toInsert = " + toInsert + "\n");
+            System.out.println("\nArray[i] = " + theArray[i] + " Array[j] = " + theArray[j] + " toInsert = " + temp + "\n");
         }
     }
 
