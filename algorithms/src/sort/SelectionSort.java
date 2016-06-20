e32a557410d38aa609bca01be55aab2bbbfd7e1a
Raul Zuniga <rzuniga64@gmail.com>
2016-06-20 18:12:55 -0500

    Update algorithms directory

--- algorithms/src/sort/SelectionSort.java
+++ algorithms/src/sort/SelectionSort.java
@@ -1,5 +1,30 @@
 package sort;
 
+/*
+ * 	Selection Sort
+ *	There is a pass for each position (0..size-1)
+ *	On each pass, the smallest (minimum) element in the rest of the list is exchanged (swapped) with element at the
+ *	current position. The first part of the list (already processed) is always sorted. Each pass increases the size
+ *	of the sorted portion
+ *
+ * 	Runtime analysis
+ * 	Best    Average     Worst
+ * 	O(n*n)  O(n*n)      O(n*n)
+ *
+ *	Array Position	0 	1	2	3	4
+ *	Initial State	36	24	10	6	12
+ *	After 1st pass	6 	24	10	36	12	6 and 36 are swapped
+ *	After 2nd pass	6 	10 	24	36	12	10 and 24 are swapped
+ *	After 3rd pass	6	10	12	36	24	12 and 24 are swapped
+ *	After 4th pass	6 	10	12	24	36	24 and 36 are swapped
+*
+*	N is the number of elements in the list
+*	Outer loop executes N-1 times
+*	Inner loop executes N-1, then N-2, then N-3, ... then once.
+*	Total number of comparisons (in inner loop) is the sum of 1 to N-1 is N(N + 1) /2
+*	Efficiency of algorith is O(N2)
+*/
+
 public class SelectionSort {
 
     private int[] theArray = new int[50];
@@ -53,7 +78,7 @@ public class SelectionSort {
     }
 
 
-    /** Selection sort search for the smallest number in the array saves it in the minimum spot and then repeats
+    /** Selection sort searches for the smallest number in the array saves it in the minimum spot and then repeats
      * searching through the entire array each time */
 
     private void selectionSort(){
@@ -78,7 +103,6 @@ public class SelectionSort {
         SelectionSort newArray = new SelectionSort();
         newArray.generateRandomArray();
         newArray.printHorizontalArray(-1,-1);
-
         newArray.selectionSort();
     }
 }
