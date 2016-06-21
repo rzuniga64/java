149ceddc385720ef8fe3cf4c3f08dc98a5c740cd
Raul Zuniga <rzuniga64@gmail.com>
2016-06-20 20:52:48 -0500

    Update algorithms directory

--- algorithms/src/sort/SelectionSort.java
+++ algorithms/src/sort/SelectionSort.java
@@ -30,53 +30,12 @@ public class SelectionSort {
     private int[] theArray = new int[50];
     private int arraySize = 10;
 
-    private void generateRandomArray(){
-        for(int i = 0; i < arraySize; i++){
-            theArray[i] = (int)(Math.random()*10)+10;
-        }
-    }
-
-    private void printHorizontalArray(int i, int j){
-
-        for(int n = 0; n < 51; n++)System.out.print("-");
-        System.out.println();
-        for(int n = 0; n < arraySize; n++)
-            System.out.print("| " + n + "  ");
-
-        System.out.println("|");
-        for(int n = 0; n < 51; n++)System.out.print("-");
-        System.out.println();
-        for(int n = 0; n < arraySize; n++)
-            System.out.print("| " + theArray[n] + " ");
-
-        System.out.println("|");
-        for(int n = 0; n < 51; n++)System.out.print("-");
-        System.out.println();
-
-        // END OF FIRST PART
-        // ADDED FOR BUBBLE SORT
-        if(j != -1){
-            // ADD THE +2 TO FIX SPACING
-            for(int k = 0; k < ((j*5)+2); k++)System.out.print(" ");
-            System.out.print(j);
-        }
-
-        // THEN ADD THIS CODE
-        if(i != -1){
-            // ADD THE -1 TO FIX SPACING
-            for(int l = 0; l < (5*(i - j)-1); l++)System.out.print(" ");
-            System.out.print(i);
-        }
-        System.out.println();
-    }
-
     private void swapValues(int indexOne, int indexTwo){
 
         int temp = theArray[indexOne];
         theArray[indexOne] = theArray[indexTwo];
         theArray[indexTwo] = temp;
     }
-
 
     /** On each pass, the smallest (minimum) element in the rest of the list is exchanged (swapped) with element at the
      *	current position. and then repeats searching through the entire array each time */
@@ -93,16 +52,15 @@ public class SelectionSort {
             }
 
             swapValues(x, minimum);
-            printHorizontalArray(x, -1);
+            SortUtility.printHorizontalArray(theArray, arraySize, x, -1);
         }
     }
-
 
     public static void main(String[] args){
 
         SelectionSort newArray = new SelectionSort();
-        newArray.generateRandomArray();
-        newArray.printHorizontalArray(-1,-1);
+        SortUtility.generateRandomArray(newArray.theArray, newArray.arraySize);
+        SortUtility.printHorizontalArray(newArray.theArray, newArray.arraySize, -1,-1);
         newArray.selectionSort();
     }
 }
