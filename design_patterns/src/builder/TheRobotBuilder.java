c62fae1c066cb1c2f814265de6b5e46f9576d416
Raul Zuniga <rzuniga64@gmail.com>
2016-06-21 20:03:41 -0500

    Update algorithms directory

--- design_patterns/src/builder/TheRobotBuilder.java
+++ design_patterns/src/builder/TheRobotBuilder.java
@@ -3,15 +3,15 @@ package builder;
 // The concrete builder class that assembles the parts
 // of the finished Robot object
 
-public class TheRobotBuilder implements RobotBuilder {
+class TheRobotBuilder implements RobotBuilder {
 
     private Robot robot;
 
-    public TheRobotBuilder() { this.robot = new Robot(); }
+    TheRobotBuilder() { this.robot = new Robot(); }
 
-    public void buildRobotHead() { robot.setRobotHead("Tin Head"); }
+    public void buildRobotHead()  { robot.setRobotHead("Tin Head"); }
     public void buildRobotTorso() { robot.setRobotTorso("Tin Torso"); }
-    public void buildRobotArms() { robot.setRobotArms("Blowtorch Arms"); }
-    public void buildRobotLegs() { robot.setRobotLegs("Rollar Skates"); }
+    public void buildRobotArms()  { robot.setRobotArms("Blowtorch Arms"); }
+    public void buildRobotLegs()  { robot.setRobotLegs("Rollar Skates"); }
     public Robot getRobot() { return this.robot; }
 }
