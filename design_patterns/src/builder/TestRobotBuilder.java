60aab41aabac0ce57e11534e780cec7f3e76a50f
Raul Zuniga <rzuniga64@gmail.com>
2016-06-21 20:40:24 -0500

    Update algorithms directory

--- design_patterns/src/builder/TestRobotBuilder.java
+++ design_patterns/src/builder/TestRobotBuilder.java
@@ -4,16 +4,16 @@ class TestRobotBuilder {
 
     public static void main(String[] args){
 
-        // Get a RobotBuilder of type TheRobotBuilder
+        // Get a specification to build a Robot
         RobotBuilder oldStyleRobot = new TheRobotBuilder();
 
-        // Pass the TheRobotBuilder specification to the engineer
+        // Pass the RobotBuilder specification to the engineer so he can build it
         RobotEngineer robotEngineer = new RobotEngineer(oldStyleRobot);
 
-        // Tell the engineer to make the Robot using the specifications of the TheRobotBuilder class
+        // The engineer makes the Robot using the specifications of the RobotBuilder class
         robotEngineer.makeRobot();
 
-        // The engineer returns the right robot based off of the spec sent to it on line 11
+        // The engineer returns the right robot based off of the specification sent to it
         Robot firstRobot = robotEngineer.getRobot();
 
         System.out.println("Robot Built");
