ebaae9fcdc3047d1d1bd8aab56fc453fdf59d9a2
Raul Zuniga <rzuniga64@gmail.com>
2016-06-23 16:28:35 -0500

    Update algorithms directory

--- design_patterns/src/bridge/TestTheRemote.java
+++ design_patterns/src/bridge/TestTheRemote.java
@@ -8,7 +8,6 @@ public class TestTheRemote{
         RemoteButton theTV2 = new TVRemotePause(new TVDevice(1, 200));
 
         RemoteButton theDVD = new DVDRemote(new DVDDevice(1,14));
-
         System.out.println("Test TV with Mute");
 
         theTV.buttonFivePressed();
