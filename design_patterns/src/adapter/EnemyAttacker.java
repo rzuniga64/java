7d05b865f99554abed7d5464e44a76b9b82b64f9
Raul Zuniga <rzuniga64@gmail.com>
2016-06-23 10:11:22 -0500

    Update algorithms directory

--- design_patterns/src/adapter/EnemyAttacker.java
+++ design_patterns/src/adapter/EnemyAttacker.java
@@ -1,12 +1,11 @@
 package adapter;
 
-// This is the Target Interface : This is what the client
-// expects to work with. It is the adapters job to make new
+// This is the Target Interface : This is what the client expects to work with. It is the adapters job to make new
 // classes compatible with this one.
 
-public interface EnemyAttacker {
+interface EnemyAttacker {
 
-    public void fireWeapon();
-    public void driveForward();
-    public void assignDriver(String driverName);
+    void fireWeapon();
+    void driveForward();
+    void assignDriver(String driverName);
 }
--- design_patterns/src/adapter/EnemyRobot.java
+++ design_patterns/src/adapter/EnemyRobot.java
@@ -1,28 +1,27 @@
 package adapter;
 
-// This is the Adaptee. The Adapter sends method calls
-// to objects that use the EnemyAttacker interface
-// to the right methods defined in EnemyRobot
+// This is the Adaptee. The Adapter sends method calls to objects that use the EnemyAttacker interface to the right
+// methods defined in EnemyRobot
 
 import java.util.Random;
 
-public class EnemyRobot{
+class EnemyRobot{
 
-    Random generator = new Random();
+    private Random generator = new Random();
 
-    public void smashWithHands() {
+    void smashWithHands() {
 
         int attackDamage = generator.nextInt(10) + 1;
         System.out.println("Enemy Robot Causes " + attackDamage + " Damage With Its Hands");
     }
 
-    public void walkForward() {
+    void walkForward() {
 
         int movement = generator.nextInt(5) + 1;
         System.out.println("Enemy Robot Walks Forward " + movement + " spaces");
     }
 
-    public void reactToHuman(String driverName) {
+    void reactToHuman(String driverName) {
 
         System.out.println("Enemy Robot Tramps on " + driverName);
     }
