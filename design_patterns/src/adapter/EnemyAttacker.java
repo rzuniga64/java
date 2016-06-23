93372f9fbdbfa0f9ef22e211bda42665357cff40
Raul Zuniga <rzuniga64@gmail.com>
2016-06-23 10:12:23 -0500

    Update algorithms directory

--- design_patterns/src/adapter/EnemyAttacker.java
+++ design_patterns/src/adapter/EnemyAttacker.java
@@ -1,6 +1,6 @@
 package adapter;
 
-// This is the Target Interface : This is what the client expects to work with. It is the adapters job to make new
+// This is the Target Interface. This is what the client expects to work with. It is the adapters job to make new
 // classes compatible with this one.
 
 interface EnemyAttacker {
