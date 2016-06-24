39afc3360c2dc08cd003a188dcb193ef24b543ae
Raul Zuniga <rzuniga64@gmail.com>
2016-06-24 01:16:37 -0500

    Update algorithms directory

---
+++ javafx/src/scenebuilder/calculator/Main.java
@@ -0,0 +1,24 @@
+package scenebuilder.calculator;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        Scene scene = new Scene(root, 400, 400);
+        scene.getStylesheets().add(getClass().getResource("calculator.css").toExternalForm());
+        primaryStage.setTitle("Calculator");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
