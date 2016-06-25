69882435905022347e1949386ded1eee455e1f24
Raul Zuniga <rzuniga64@gmail.com>
2016-06-25 15:37:39 -0500

    Update algorithms directory

---
+++ javafx/src/scenebuilder/menubar/Controller.java
@@ -0,0 +1,27 @@
+package scenebuilder.menubar;
+
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.TreeView;
+
+public class Controller implements Initializable {
+
+    @FXML
+    MenuBar menuBar;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void CloseApp(ActionEvent event) {
+        Platform.exit();
+        System.exit(0);
+    }
+}
