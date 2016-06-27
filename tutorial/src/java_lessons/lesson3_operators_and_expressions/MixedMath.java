3beff0c1f4cad450ac9d315f0bc42188d96747ab
Raul Zuniga <rzuniga64@gmail.com>
2016-06-26 21:32:43 -0500

    Update algorithms directory

--- tutorial/src/java_lessons/lesson3_operators_and_expressions/MixedMath.java
+++ tutorial/src/java_lessons/lesson3_operators_and_expressions/MixedMath.java
@@ -5,7 +5,7 @@ public class MixedMath
    public static void main(String[] args)
    {
       double dnum;
-      int num = 2;;
+      int num = 2;
       dnum = num;
       System.out.println(dnum);  
       
