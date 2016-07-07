528b154fd3b0f5130a3b62e19ac262c33e0aaeea
Raul Zuniga <rzuniga64@gmail.com>
2016-07-06 22:49:43 -0500

    Update algorithms directory

--- tutorial/src/java_lessons/lesson15_generic_programming/List.java
+++ tutorial/src/java_lessons/lesson15_generic_programming/List.java
@@ -1,26 +1,34 @@
 package java_lessons.lesson15_generic_programming;
 
-// We want to specify a generic data type using generic programming
-// by using a type place holder <T>.
+/**
+ * We want to specify a generic data type using generic programming by using a type place holder <T>.
+ *
+ * @param <T> the type parameter
+ */
+
 public class List<T> {
     private T[] data;
     private int size;
     private int capacity;
 
-    public List(int numElements) {
+    /**
+     * Instantiates a new List.  Generic classes and methods only work with Object data types.
+     * Data types must be Object instead of primitive data types. Create a new Object array and cast it to the data type
+     * that will be provided when we instantiate a list.
+     *
+     * @param numElements the num elements
+     */
+    List(int numElements) {
         size = numElements;
         capacity = 0;
-
-        /* Generic classes and methods only work with Object
-           data types. Data types must be Object instead
-           of primitive data types.
-
-           create a new Object array and cast it to the data
-           type that will be provided when we instantiate a list
-        */
         data = (T[]) new Object[size];
     }
 
+    /**
+     * This method will add a generic data type to a data list.
+     *
+     * @param element the element to add
+     */
     public void add(T element) {
         data[capacity] = element;
         capacity++;
