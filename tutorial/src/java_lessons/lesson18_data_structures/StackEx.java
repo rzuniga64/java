8568173ff0ad6703d96fad57dd203664ed62300c
Raul Zuniga <rzuniga64@gmail.com>
2016-07-08 14:49:53 -0500

    Update algorithms directory

--- tutorial/src/java_lessons/lesson18_data_structures/StackEx.java
+++ tutorial/src/java_lessons/lesson18_data_structures/StackEx.java
@@ -2,27 +2,38 @@ package java_lessons.lesson18_data_structures;
 
 import java.util.Stack;
 
-// items stored in a stack is stored as an Object data type.
-// so the data type does not need to be specified.
+/**
+ * The type Stack ex. Items stored in a stack is stored as an Object data type so the data type does not need to be
+ * specified.
+ */
 public class StackEx {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
+
         Stack names = new Stack();
+
         names.push("Raymond");
         names.push("David");
+
         System.out.println("Top of stack: " + names.peek());
         names.pop();
         System.out.println("Top of stack: " + names.peek());
         names.push("Cynthia");
         System.out.println("Top of stack: " + names.peek());
-        if (!names.empty()) {
+
+        if (!names.empty())
             names.pop();
-        }
+
         System.out.println("Top of stack: " + names.peek());
         names.pop();
-        if (!names.empty()) {
+
+        if (!names.empty())
             System.out.println("Top of stack: " + names.peek());
-        } else {
+         else
             System.out.println("Stack empty.");
-        }
     }
 }
