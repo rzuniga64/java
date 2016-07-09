d3eff3a30a5b3957e1e2391b0c8aabd7e7a55c5a
Raul Zuniga <rzuniga64@gmail.com>
2016-07-09 03:02:53 -0500

    Update algorithms directory

--- tutorial/src/java_lessons/lesson19_sort_and_search/sort/InsertionSort.java
+++ tutorial/src/java_lessons/lesson19_sort_and_search/sort/InsertionSort.java
@@ -1,27 +1,65 @@
 package java_lessons.lesson19_sort_and_search.sort;
 
-
 /**
  * The type Insertion sort.
  */
 public class InsertionSort {
 
     /**
-     * this method performs an Insertion sort.
+     *  This method performs an Insertion sort.
      *
-     * @param data the data to sort.
-     */
-    static void insertionSort(int [] data) {
+     *  @param data the data to sort.
+     *  The Insertion sort.SelectionSort is normally the best of the elementary sorts.
+     *  Unlike the other sorts that had a group sorted at any given time, groups are only partially sorted here.
+     *
+     *  Runtime analysis
+     *  Best    Average     Worst
+     *  O(n)    O(n*n)      O(n*n)
+     *
+     * Insertion sort is a simple sorting algorithm that is appropriate for small inputs
+     * The basic action of insertion sort is to sort the elements in positions 0 through p (where p ranges from 1 through
+     * N-1). In each state p increases by 1. That is what the outer loop is controlling.
+     *
+     * When the body of the for loop is entered we are guaranteed that the elements in array positions 0 through p-1 have
+     * already been sorted and that we need to extend this to positions 0 to p.
+     *
+     * At each step the underlined element needs to be added to the previously sorted part of the array. We can easily
+     * do that by placing it in a temporary variable and sliding all the elements that ARE LARGER THAN IT on position
+     * to the right. We then copy the temporary variable into the former position of the leftmost relocated
+     * element. We keep a counter j, which is the position in which the temporary variable should be written back.
+     * Every time an element is slid, j decreases by 1.
+     *
+     * Array Position           0   1   2   3   4   5
+     * ----------------------------------------------
+     * Initial state            8|  5   9   2   6   3   temp = 5
+     *                              -
+     * After a[0..1] is sorted  5   8|  9   2   6   3   temp = 9
+     *                                  -
+     * After a[0..2] is sorted  5   8   9|  2   6   3   temp = 2
+     *                                      -
+     * After a[0..3] is sorted  2   5   8   9|  6   3   temp = 6
+     *                                          -
+     * After a[0..4] is sorted  2   5   6   8   9|  3   temp = 3
+     *                                              -
+     * After a[0..5] is sorted  2   3   5   6   8   9|
+     **/
+    static void insertionSort(int[] data) {
+
         int size = data.length;
+        int temp = 0;
+        int j;
 
         for (int i = 0; i < size; i++) {
-            for (int j = i; j > 0; --j) {
+            for (j = i; j > 0; --j) {
                 if (data[j-1] > data[j]) {
-                    int temp = data[j];
+                    temp = data[j];
                     data[j] = data[j-1];
+                    SortUtility.printHorizontalArray(data, size, i, j);
                     data[j-1] = temp;
                 }
             }
+            SortUtility.printHorizontalArray(data, size, i, j);
+            System.out.println("\nArray[i] = " + data[i] + " Array[j] = " + data[j] + " toInsert = " + temp + "\n");
         }
     }
 
@@ -39,8 +77,9 @@ public class InsertionSort {
             data[i] = (int) (Math.random() * 100);
 
         System.out.println("Before sorting: ");
-        for (int i = 0; i < size; i++)
-            System.out.println(data[i]);
+       // for (int i = 0; i < size; i++)
+            //System.out.println(data[i]);
+        SortUtility.printHorizontalArray(data, size, -1,-1);
 
         insertionSort(data);
 
---
+++ tutorial/src/java_lessons/lesson19_sort_and_search/sort/SortUtility.java
@@ -0,0 +1,105 @@
+package java_lessons.lesson19_sort_and_search.sort;
+
+public class SortUtility {
+
+    // Fills the Array with random values
+    public static void generateRandomArray(int[] theArray, int arraySize){
+
+        for(int i = 0; i < arraySize; i++)
+            theArray[i] = (int)(Math.random()*90)+10;
+    }
+
+    // Prints the Array on the screen in a grid
+    public static void printArray(int[] theArray, int arraySize){
+
+        System.out.println("----------");
+        for(int i = 0; i < arraySize; i++){
+            System.out.print("| " + i + " | ");
+            System.out.println(theArray[i] + " |");
+            System.out.println("----------");
+        }
+    }
+
+    public static void printHorizontalArray(int[] theArray, int arraySize, int i, int j){
+
+        for(int n = 0; n < 51; n++)System.out.print("-");
+        System.out.println();
+        for(int n = 0; n < arraySize; n++)
+            System.out.print("| " + n + "  ");
+
+        System.out.println("|");
+        for(int n = 0; n < 51; n++)System.out.print("-");
+        System.out.println();
+        for(int n = 0; n < arraySize; n++)
+            System.out.print("| " + theArray[n] + " ");
+
+        System.out.println("|");
+        for(int n = 0; n < 51; n++)System.out.print("-");
+        System.out.println();
+
+        // END OF FIRST PART
+        // ADDED FOR BUBBLE SORT
+        if(j != -1){
+            // ADD THE +2 TO FIX SPACING
+            for(int k = 0; k < ((j*5)+2); k++)System.out.print(" ");
+            System.out.print(j);
+        }
+
+        // THEN ADD THIS CODE
+        if(i != -1){
+            // ADD THE -1 TO FIX SPACING
+            for(int l = 0; l < (5*(i - j)-1); l++)System.out.print(" ");
+            System.out.print(i);
+        }
+        System.out.println();
+    }
+
+    public static void printHorzArray(int[] theArray, int arraySize, int i, int j, int h) {
+        if (i == j)
+            i = i - h;
+
+        for (int n = 0; n < 51; n++)
+            System.out.print("-");
+
+        System.out.println();
+
+        for (int n = 0; n < arraySize; n++)
+            System.out.print("|  " + n + " ");
+
+        System.out.println("|");
+
+        for (int n = 0; n < 51; n++)
+            System.out.print("-");
+
+        System.out.println();
+
+        for (int n = 0; n < arraySize; n++)
+            System.out.print("| " + theArray[n] + " ");
+
+        System.out.println("|");
+
+        for (int n = 0; n < 51; n++)
+            System.out.print("-");
+
+        System.out.println();
+
+        if (i != -1) {
+            // Number of spaces to put before the F
+            int spacesBeforeFront = 5 * i + 4;
+
+            for (int k = 0; k < spacesBeforeFront; k++)
+                System.out.print(" ");
+
+            System.out.print("I");
+
+            // Number of spaces to put before the R
+            int spacesBeforeRear = (5 * j + 1 - 1) - spacesBeforeFront;
+
+            for (int l = 0; l < spacesBeforeRear; l++)
+                System.out.print(" ");
+
+            System.out.print("O");
+            System.out.println("\n");
+        }
+    }
+}
