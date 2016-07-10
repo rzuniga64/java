0cc6ce1e74809e553f925123514488f6cda061d0
Raul Zuniga <rzuniga64@gmail.com>
2016-07-10 14:14:26 -0500

    Update algorithms directory

--- tutorial/src/java_lessons/lesson21_jdbc/TestDriver.java
+++ tutorial/src/java_lessons/lesson21_jdbc/TestDriver.java
@@ -2,7 +2,15 @@ package java_lessons.lesson21_jdbc;
 
 import java.sql.*;
 
+/**
+ * The type Test driver. This class is used to test that the Connector/J is installed properly.
+ */
 public class TestDriver {
+    /**
+     * Main.
+     *
+     * @param args the args
+     */
     public static void main(String args[]) {
         try {
             Driver driver = (Driver)Class.forName("com.mysql.jdbc.Driver").newInstance();
