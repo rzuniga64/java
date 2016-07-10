e42fce89832428c475927a37827b0ec4fbe3021c
Raul Zuniga <rzuniga64@gmail.com>
2016-07-10 18:45:13 -0500

    Update algorithms directory

--- tutorial/src/java_lessons/lesson21_jdbc/ConnectToDB.java
+++ tutorial/src/java_lessons/lesson21_jdbc/ConnectToDB.java
@@ -4,34 +4,47 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * The type Connect to db.
+ */
 public class ConnectToDB {
-    // authentication details for the database
-    private static final String username ="root";
-    private static final String password = "aggies92";
-    private static final String dbName = "test";
 
-    /*
-        getConnection returns a reference to the Connection object
-        for the database
+    // authentication details for the database
+    private static final String username ="rzuniga64";
+    private static final String password = "aggies92";
+    private static final String url = "jdbc:mysql://localhost:3306/test";
+
+    /**
+     * This method creates a connection to a database named 'test'.
+     *
+     * @return the connection
      */
     public static Connection getConnection() {
+
         // Create a connection object with global scope
         Connection connection = null;
         try {
-            // Load MySQL Connector/J driver
+            // Create an instance of the MySQL Connector/J driver
             Class.forName("com.mysql.jdbc.Driver").newInstance();
 
-            // Get the MySQL connection
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + dbName, username, password);
+            // getConnection returns a reference to the Connection object for the database
+            connection = DriverManager.getConnection(url, username, password);
             System.out.println("Database connection successful");
         } catch (SQLException e) {
             System.out.println("Cannot connect to database.");
         } catch (Exception e) {
             System.out.println("Connection failed.");
+        } finally {
+            closeConnection(connection);
         }
     return connection;
     }
 
+    /**
+     * This method closes the MySQL database connection.
+     *
+     * @param connection the connection
+     */
     static void closeConnection(Connection connection) {
         if (connection != null) {
             try {
@@ -43,8 +56,12 @@ public class ConnectToDB {
         }
     }
 
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         Connection connection = getConnection();
-        closeConnection(connection);
     }
 }
