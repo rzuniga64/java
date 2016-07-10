0bc7f5598c937ad9dec8888de1b5923e4843bb6f
Raul Zuniga <rzuniga64@gmail.com>
2016-07-10 04:05:13 -0500

    Update algorithms directory

--- tutorial/src/java_lessons/lesson12_arrays/ArrayStructures.java
+++ tutorial/src/java_lessons/lesson12_arrays/ArrayStructures.java
@@ -5,7 +5,7 @@ public class ArrayStructures {
     private int arraySize = 10; // Elements in theArray
 
     // Fills the Array with random values
-    public void generateRandomArray(){
+    private void generateRandomArray(){
         for(int i = 0; i < arraySize; i++){
             // Random number 10 through 19
             theArray[i] = (int)(Math.random()*10)+10;
@@ -20,7 +20,7 @@ public class ArrayStructures {
     }
 
     // Prints the Array on the screen in a grid
-    public void printArray() {
+    private void printArray() {
         System.out.println("----------");
         for (int i = 0; i < arraySize; i++) {
             System.out.print("| " + i + " | ");
@@ -30,13 +30,13 @@ public class ArrayStructures {
     }
 
     // Gets value at provided index
-    public int getValueAtIndex(int index){
+    private int getValueAtIndex(int index){
         if(index < arraySize) return theArray[index];
         return 0;
     }
 
     // Returns true or false if a value is in the Array
-    public boolean doesArrayContainThisValue(int searchValue){
+    private boolean doesArrayContainThisValue(int searchValue){
         boolean valueInArray = false;
         for(int i = 0; i < arraySize; i++){
             if(theArray[i] == searchValue)
@@ -46,7 +46,7 @@ public class ArrayStructures {
     }
 
     // Delete Array row for the index and move elements up
-    public void deleteIndex(int index){
+    private void deleteIndex(int index){
         if(index < arraySize){
 
             // Overwrite the value for the supplied index
@@ -60,7 +60,7 @@ public class ArrayStructures {
         }
     }
 
-    public void insertValue(int value){
+    private void insertValue(int value){
         if(arraySize < 50){
             theArray[arraySize] = value;
             arraySize++;
