148fb6943fe3d5f5a84a241434af84ccf81ef02a
Raul Zuniga <rzuniga64@gmail.com>
2016-07-10 04:00:21 -0500

    Update algorithms directory

--- tutorial/src/java_lessons/lesson19_sort_and_search/sort/ShellSort.java
+++ tutorial/src/java_lessons/lesson19_sort_and_search/sort/ShellSort.java
@@ -4,32 +4,10 @@ import java.util.Arrays;
 
 import static java_lessons.lesson19_sort_and_search.sort.SortUtility.*;
 
-/**
- *  Shell sort is a sub quadratic algorithm whose code is only slightly longer than the insertion sort, making it the
- *  simplest of the faster algorithms. It compares elements that are far apart and then by comparing elements that are
- *  less far apart, and so on, gradually shrinking toward the basic insertion sort.  Shell sort is known as a
- *  diminishing gap sort. When gap is 1 the loop is identical to an insertion sort.
- *
- *  The running time of Shell sort depends heavily on the choice of increment sequences.  A sequence which performs well
- *  in practice but has no theoretical basis, is to have a starting gap of N/2.2. This divisor appears to bring the
- *  average running time to below O(pow(N, 5/4)).  For 100, 000, to 1,000,000 items, it typically improves performance
- *  by about to 25 to 35 percent over Shell's Increments, although nobody knows why.  The performance of Shell sort is
- *  quite acceptable in practice, even for N in the tens of thousands. The simplicity of the code makes it the algorithm
- *  of choice for sorting up to moderately large input.  it is also a fine example of a very simple algorithm with an
- *  extremely complex analysis.
- *
- *  Runtime analysis
- *  Best        Average         Worst
- *  O(n log n)  depends on gap  O(n log n * log n)
- *
- *  Original        81  94  11  96  12  35  17  95  28  58  41  75  15
- *  After 5-sort    35  17  11  28  12  41  75  15  96  58  81  94  95
- *                  --                  --                  --
- *  After 3-sort    28  12  11  35  15  41  58  17  94  75  81  96  95
- *                  --          --          --          --          --
- *  After 1-sort    11  12  15  17  28  35  41  58  75  81  94  95  96
- */
 
+/**
+ * The type Shell sort.
+ */
 public class ShellSort {
 
     private int[] theArray;
@@ -41,6 +19,31 @@ public class ShellSort {
         generateRandomArray(this.theArray, this.arraySize);
     }
 
+    /**
+     *  Shell sort is a sub quadratic algorithm whose code is only slightly longer than the insertion sort, making it the
+     *  simplest of the faster algorithms. It compares elements that are far apart and then by comparing elements that are
+     *  less far apart, and so on, gradually shrinking toward the basic insertion sort.  Shell sort is known as a
+     *  diminishing gap sort. When gap is 1 the loop is identical to an insertion sort.
+     *
+     *  The running time of Shell sort depends heavily on the choice of increment sequences.  A sequence which performs well
+     *  in practice but has no theoretical basis, is to have a starting gap of N/2.2. This divisor appears to bring the
+     *  average running time to below O(pow(N, 5/4)).  For 100, 000, to 1,000,000 items, it typically improves performance
+     *  by about to 25 to 35 percent over Shell's Increments, although nobody knows why.  The performance of Shell sort is
+     *  quite acceptable in practice, even for N in the tens of thousands. The simplicity of the code makes it the algorithm
+     *  of choice for sorting up to moderately large input.  it is also a fine example of a very simple algorithm with an
+     *  extremely complex analysis.
+     *
+     *  Runtime analysis
+     *  Best        Average         Worst
+     *  O(n log n)  depends on gap  O(n log n * log n)
+     *
+     *  Original        81  94  11  96  12  35  17  95  28  58  41  75  15
+     *  After 5-sort    35  17  11  28  12  41  75  15  96  58  81  94  95
+     *                  --                  --                  --
+     *  After 3-sort    28  12  11  35  15  41  58  17  94  75  81  96  95
+     *                  --          --          --          --          --
+     *  After 1-sort    11  12  15  17  28  35  41  58  75  81  94  95  96
+     */
     private void shellsort() {
         long startTime;
         long endTime;
@@ -68,6 +71,11 @@ public class ShellSort {
         System.out.println("Shellsort took " + (endTime - startTime) + " milliseconds.");
     }
 
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         ShellSort theSort = new ShellSort(10);
         System.out.println(Arrays.toString(theSort.theArray));
