f3345b408a2a64d5298589c5cf05b7f7d16ce381
Raul Zuniga <rzuniga64@gmail.com>
2016-07-10 02:09:24 -0500

    Update algorithms directory

--- tutorial/src/java_lessons/lesson19_sort_and_search/sort/MergeSort.java
+++ tutorial/src/java_lessons/lesson19_sort_and_search/sort/MergeSort.java
@@ -7,12 +7,59 @@ package java_lessons.lesson19_sort_and_search.sort;
 public class MergeSort {
 
     /**
-     * this method peforms a Merge sort.
+     *  This method performs a Merge sort.
      *
-     * @param arr  the arr
-     * @param low  the low
-     * @param high the high
+     *  @param arr  the arr
+     *  @param low  the low
+     *  @param high the high
+     *
+     *  Runtime analysis
+     *  Best        Average     Worst
+     *  O(n log n)  O(n log n)      O(n log n)
+     *
+     *  Merge sort has fewer comparisons compared with Quick sort but more swapping (copying).
+     *  In Java, comparing objects can be expensive while swapping (copying) objects is relatively cheap.
+     *  Therefore, Merge sort is the sorting routine commonly used in Java libraries.
+     *
+     *  Merge sort is an efficient, general-purpose, divide and conquer sorting algorithm.
+     *  Merge sort works by splitting up the array into individual pieces. It compares adjacent elements and puts them
+     *  in order. Next it looks at adjacent elements of each pair and orders those.  Finally, both parts are merged and
+     *  sorted into their final correct order.
+     *
+     *  Array Position           0   1   2   3   4   5
+     *  ----------------------------------------------
+     *  Initial state            6    5   3   1   8   7   2   4
+     *
+     *  After 1st pass sorted    5   5  | 1   3 | 7   8 | 2   4     sort each pair
+     *
+     *  After 2nd pass sorted    1   3    5   6 | 2   4   7   8     sort adjacent elements of each pair
+     *
+     *  After 3rd pass sorted    1   2    3   4   5   6   7   8     both parts merged and sorted
+     **/
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
      */
+    public static void main(String[] args) {
+
+        int size = 10;
+        int[] data = new int[]{5,10,1,6,2,9,3,8,7,4};
+        int low = 0;
+        int high = 9;
+
+        System.out.println("Before sorting: ");
+        for (int i = 0; i < size; i++)
+            System.out.println(data[i]);
+
+        mergeSort(data, low, high);
+
+        System.out.println("\nAfter sorting: ");
+        for (int i = 0; i < size; i++)
+            System.out.println(data[i]);
+    }
+
     static void mergeSort(int[] arr, int low, int high) {
 
         int size = arr.length;
@@ -54,28 +101,5 @@ public class MergeSort {
             ++k;
             ++i;
         }
-    }
-
-    /**
-     * The entry point of application.
-     *
-     * @param args the input arguments
-     */
-    public static void main(String[] args) {
-
-        int size = 10;
-        int[] data = new int[]{5,10,1,6,2,9,3,8,7,4};
-        int low = 0;
-        int high = 9;
-
-        System.out.println("Before sorting: ");
-        for (int i = 0; i < size; i++)
-            System.out.println(data[i]);
-
-        mergeSort(data, low, high);
-
-        System.out.println("\nAfter sorting: ");
-        for (int i = 0; i < size; i++)
-            System.out.println(data[i]);
     }
 }
