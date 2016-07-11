1a0582d1d0308e21130b2a0a7ade5ad2d6ccc47a
Raul Zuniga <rzuniga64@gmail.com>
2016-07-10 19:43:20 -0500

    Update algorithms directory

--- tutorial/src/java_lessons/lesson21_jdbc/InsertData.java
+++ tutorial/src/java_lessons/lesson21_jdbc/InsertData.java
@@ -18,25 +18,12 @@ public class InsertData {
         Connection connection = conn.getConnection();
         try {
             Statement stmt; // use to formulate SQL
-            ResultSet result;
             stmt = connection.createStatement();
             stmt.executeUpdate("insert into books(isbn, title, author, publisher) values" +
                                 "('007160630', 'Java-The Complete Reference','Herbert Schildt', 'Oracle Press')," +
                                 "('032135668', 'Effective Java', 'Joshua Bloch', 'Addison-Wesley')");
             System.out.println("Record added");
-            result = stmt.executeQuery("select * from books");
-            while (result.next()) {
-                String isbn = result.getString("isbn");
-                String title = result.getString("title");
-                String author = result.getString("author");
-                String publisher = result.getString("publisher");
 
-                System.out.print("ISBN: " + isbn);
-                System.out.println(" Title: " + title);
-                System.out.print("Author: " + author);
-                System.out.println(" Publisher: " + publisher);
-            }
-            result.close();
             conn.closeConnection(connection);
         } catch (Exception e) {
             System.out.println("Error inserting record");
--- tutorial/src/java_lessons/lesson21_jdbc/UpdateData.java
+++ tutorial/src/java_lessons/lesson21_jdbc/UpdateData.java
@@ -4,28 +4,25 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+/**
+ * The type UpdateData.
+ */
 public class UpdateData {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         ConnectToDB conn = new ConnectToDB();
         Connection connection = conn.getConnection();
         try {
             Statement stmt; // use to formulate SQL
-            ResultSet result;
+
             stmt = connection.createStatement();
             stmt.executeUpdate("update books set author = 'Raul Zuniga' where ISBN = '032135668'");
-            System.out.println("Record added");
-            result = stmt.executeQuery("select * from books");
-            while (result.next()) {
-                String isbn = result.getString("isbn");
-                String title = result.getString("title");
-                String author = result.getString("author");
-                String publisher = result.getString("publisher");
-                System.out.print("ISBN: " + isbn);
-                System.out.println(" Title: " + title);
-                System.out.print("Author: " + author);
-                System.out.println(" Publisher: " + publisher);
-            }
-            result.close();
+            System.out.println("Record updated");
+
             conn.closeConnection(connection);
         } catch (Exception e) {
             System.out.println("Error updating record");
