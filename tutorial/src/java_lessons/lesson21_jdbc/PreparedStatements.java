de64387be1af9c6bba00c0c710f7c95379ee8ea8
Raul Zuniga <rzuniga64@gmail.com>
2016-07-10 19:55:41 -0500

    Update algorithms directory

--- tutorial/src/java_lessons/lesson21_jdbc/PreparedStatements.java
+++ tutorial/src/java_lessons/lesson21_jdbc/PreparedStatements.java
@@ -1,36 +1,41 @@
 package java_lessons.lesson21_jdbc;
 
+import javafx.beans.property.SimpleStringProperty;
+
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.sql.PreparedStatement;
 
-// Prepared statements allow use of placeholders
-public class Prepared {
+/**
+ * This class uses prepared statements which allow use of placeholders rather than concatenate data together to build
+ * our SQL. Create the prepared statement as a String. Then call the prepareStatement method of the Connection object
+ * with that String to build a prepared statement. Then use a set method to call on your preparedStatement object. Then
+ * once all the data values are set then call executeUpdate method.
+ */
+//
+public class PreparedStatements {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
+
         ConnectToDB conn = new ConnectToDB();
         Connection connection = conn.getConnection();
+
         try {
             Statement stmt; // use to formulate SQL
             ResultSet result;
             String ps = "update books set title = ? where isbn = ?";
+
             PreparedStatement ptst = connection.prepareStatement(ps);
+            // prepare data to be put in place of the placeholders.
             ptst.setString(1, "Effective Java Eighth Edition");
             ptst.setString(2,"032135668");
             ptst.executeUpdate();
-            stmt = connection.createStatement();
-            result = stmt.executeQuery("select * from books");
-            while (result.next()) {
-                String isbn = result.getString("isbn");
-                String title = result.getString("title");
-                String author = result.getString("author");
-                String publisher = result.getString("publisher");
-                System.out.print("ISBN: " + isbn);
-                System.out.println(" Title: " + title);
-                System.out.print("Author: " + author);
-                System.out.println(" Publisher: " + publisher);
-            }
-            result.close();
+
             conn.closeConnection(connection);
         } catch (Exception e) {
             System.out.println("Error updating record");
