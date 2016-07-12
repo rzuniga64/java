a0ca4754c59580d8c3bda2d91965662233045caa
Raul Zuniga <rzuniga64@gmail.com>
2016-07-12 13:19:36 -0500

    Update algorithms directory

--- tutorial/src/java_lessons/lesson30_regex/RegEx2.java
+++ tutorial/src/java_lessons/lesson30_regex/RegEx2.java
@@ -1,18 +1,32 @@
 package java_lessons.lesson30_regex;
 
-// metacharacters  - shortcut for a character class: \w word \d digit \s space
-// \w matches any a-z or 0-9 character
 import java.util.regex.*;
 
+/**
+ *  The type RegEx2.
+ *
+ *  meta characters  - shortcut for a character class:
+ *  \w word any a-z or 0-9 character
+ *  \d digit
+ *  \s space
+ */
 public class RegEx2 {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
+
         String words = ("90 percent effort 100 percent of the time");
-        //String patt = "\\w+";
-        //String patt = "\\d+";
-        String patt = "\\s";
+        //String patt = "\\w+"; // words
+        //String patt = "\\d+"; // digits
+        String patt = "\\s";    // spaces
+
         Pattern pattern = Pattern.compile(patt);
         Matcher match = pattern.matcher(words);
         System.out.println(words);
+
         if (!patt.equals("\\s")) {
             while (match.find()) {
                 System.out.println("Found " + patt + " at: " + match.start());
