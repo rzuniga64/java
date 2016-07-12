ddc5f6536ecc0ca43ec6f086c34dba17e4935e38
Raul Zuniga <rzuniga64@gmail.com>
2016-07-12 13:22:13 -0500

    Update algorithms directory

--- tutorial/src/java_lessons/lesson30_regex/RegEx3.java
+++ tutorial/src/java_lessons/lesson30_regex/RegEx3.java
@@ -2,12 +2,23 @@ package java_lessons.lesson30_regex;
 
 import java.util.regex.*;
 
+/**
+ * The type RegEx3.
+ */
 public class RegEx3 {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
+
         String names = ("Mike McMillian Terri McMillen Allison McMillin ");
         String patt = "McM.*? ";
+
         Pattern pattern = Pattern.compile(patt);
         Matcher match = pattern.matcher(names);
+
         System.out.println(names);
         names = match.replaceAll("McMillan ");
         System.out.println(names);
