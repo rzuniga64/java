fbffaaf2c8e9a4145526dcfe1021cb0d456061d7
Raul Zuniga <rzuniga64@gmail.com>
2016-11-15 01:53:09 -0600

    Update HashTable example to use the Division method for the hash function.

--- algorithms/src/hash_table/HashTable.java
+++ algorithms/src/hash_table/HashTable.java
@@ -7,116 +7,108 @@ import java.util.Arrays;
  *  array. The position the item goes in is known as the slot. Hashing doesn't work very well in situations in which
  *  duplicate data is stored. Also it isn't good for searching for anything except a specific key. However a Hash Table
  *  is a data structure that offers fast insertion and searching capabilities.
+ *
+ *  PUBLIC OPERATIONS
+ *  int hashFunction(x)         : find the index into the hash table using x
+ *  void insert(x)              : Insert x
+ *  void remove(x)              : Remove x
+ *  HashEntry find(x)           : Return item and it's index that matches x
+ *  void displayTheStack()
  */
+public class HashTable  {
 
-public class HashTable {
+    private class HashEntry {
+        private int key;
+        private String value;
 
-    private String[] theArray;
-    private int arraySize;
+        HashEntry(int key, String value) {
+            this.key = key;
+            this.value = value;
+        }
 
-    public static void main(String[] args) {
+        public int getKey() {
+            return key;
+        }
 
-        HashTable theFunc = new HashTable(30);
-
-        // Simplest Hash Function
-        // String[] elementsToAdd = { "1", "5", "17", "21", "26" };
-        // theFunc.hashFunction1(elementsToAdd, theFunc.theArray);
-
-        // Mod Hash Function
-        // This contains exactly 30 items to show how collisions will work
-
-        String[] elementsToAdd2 = { "100", "510", "170", "214", "268", "398",
-                "235", "802", "900", "723", "699", "1", "16", "999", "890",
-                "725", "998", "978", "988", "990", "989", "984", "320", "321",
-                "400", "415", "450", "50", "660", "624" };
-
-        theFunc.hashFunction2(elementsToAdd2, theFunc.theArray);
-
-        // Locate the value 660 in the Hash Table
-        theFunc.findKey("660");
-        theFunc.displayTheStack();
-    }
-
-    // Simple Hash Function that puts values in the same index that matches their value
-
-    public void hashFunction1(String[] stringsForArray, String[] theArray) {
-
-        for (int n = 0; n < stringsForArray.length; n++) {
-
-            String newElementVal = stringsForArray[n];
-            theArray[Integer.parseInt(newElementVal)] = newElementVal;
+        public String getValue() {
+            return value;
         }
     }
 
-    // Now let's say we have to hold values between 0 & 999 but we never plan to have more than 15 values in all.
-    // It wouldn't make sense to make a 1000 item array, so what can we do?
+    private HashEntry[] table;
+    private int tableSize;
+    public static int occupied;
 
-    // One way to fit these numbers into a 30 item array is to use the mod function. All you do is take the modulus
-    // of the value versus the array size.
-
-    // The goal is to make the array big enough to avoid collisions, but not so big that we waste memory.
-
-    private void hashFunction2(String[] stringsForArray, String[] theArray) {
-
-        for (int n = 0; n < stringsForArray.length; n++) {
-            String newElementVal = stringsForArray[n];
-
-            // Create an index to store the value in by taking the modulus
-            int arrayIndex = Integer.parseInt(newElementVal) % 29;
-            System.out.println("Modulus Index= " + arrayIndex + " for value " + newElementVal);
-
-            // Cycle through the array until we find an empty space
-            while (!theArray[arrayIndex].equals("-1")) {
-
-                ++arrayIndex;
-                System.out.println("Collision Try " + arrayIndex + " Instead");
-
-                // If we get to the end of the array go back to index 0
-                arrayIndex %= arraySize;
-            }
-
-            theArray[arrayIndex] = newElementVal;
-        }
-    }
-
-    // Returns the value stored in the Hash Table
-    private String findKey(String key) {
-
-        // Find the keys original hash key
-        int arrayIndexHash = Integer.parseInt(key) % 29;
-
-        while (!theArray[arrayIndexHash].equals("-1")) {
-
-            if (theArray[arrayIndexHash].equals(key)) {
-                // Found the key so return it
-                System.out.println(key + " was found in index " + arrayIndexHash);
-
-                return theArray[arrayIndexHash];
-            }
-
-            // Look in the next index
-            ++arrayIndexHash;
-
-            // If we get to the end of the array go back to index 0
-            arrayIndexHash %= arraySize;
-        }
-
-        // Couldn't locate the key
-        return null;
-    }
-
+    /**
+     * Constructor
+     * Returns the value stored in the Hash Table
+     *
+     * @param size the size of the hash table
+     */
     private HashTable(int size) {
 
-        arraySize = size;
-        theArray = new String[size];
-        Arrays.fill(theArray, "-1");
+        tableSize = size;
+        table = new HashEntry[tableSize];
+        Arrays.fill(table, null);
     }
 
+    /**
+     *  Division Method
+     *
+     *  Create a hashing function using the remainder of the key divided by the table size as the index for the given
+     *  element. This function could be defined as follows: Hashcode(key) = Math.abs(key)%p This function will yield a
+     *  result in the range from 0 to tableSize-1 so have an index that maps directly to a location in the table.
+     *  The goal is to make the array big enough to avoid collisions, but not so big that we waste memory.
+     *
+     * @param value the value to store in the hash table
+     */
+    private int hashFunction(String value) {
+
+            // Create an index to store the value in by taking the modulus
+            int hash = Math.abs(Integer.parseInt(value)) % this.tableSize;
+            System.out.println("Modulus Index= " + hash + " for value " + value);
+
+            // Cycle through the array until we find an empty slot.
+            while (table[hash] != null && !table[hash].getValue().equals(value)) {
+
+                ++hash;
+                System.out.println("Collision Try " + hash + " Instead");
+                occupied++;
+
+                // If we get to the end of the array go back to index 0
+                hash %= tableSize;
+            }
+
+            return hash;
+    }
+
+    private void insert(String value) {
+        int currentPos = hashFunction(value);
+
+        table[currentPos] = new HashEntry(currentPos, value);
+    }
+
+    /**
+     * Returns the value stored in the Hash Table
+     *
+     * @param value the value to find
+     */
+    private HashEntry find(String value) {
+
+        int currentPos = hashFunction(value);
+
+        return table[currentPos];
+    }
+
+
+    /**
+     * Display the hash table.
+     */
     private void displayTheStack() {
 
         int increment = 0;
 
-        for (int m = 0; m < 3; m++) {
+        for (int m = 0; m < 4; m++) {
 
             increment += 10;
 
@@ -137,10 +129,10 @@ public class HashTable {
 
             for (int n = increment - 10; n < increment; n++) {
 
-                if (theArray[n].equals("-1"))
+                if (table[n] == null)
                     System.out.print("|      ");
                 else
-                    System.out.print(String.format("| %3s " + " ", theArray[n]));
+                    System.out.print(String.format("| %3s " + " ", table[n].getValue()));
             }
 
             System.out.println("|");
@@ -151,4 +143,34 @@ public class HashTable {
             System.out.println();
         }
     }
+
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+
+        final int TABLE_SIZE = 43;
+
+        HashTable table = new HashTable(TABLE_SIZE);
+
+        // This contains exactly 30 items to show how collisions will work
+
+        String[] valuesArray = { "100", "510", "170", "214", "268", "398",
+                "235", "802", "900", "723", "699", "1", "16", "999", "890",
+                "725", "998", "978", "988", "990", "989", "984", "320", "321",
+                "400", "415", "450", "50", "660", "624" };
+
+        for (String value : valuesArray)
+            table.insert( value );
+
+        // Locate the value 660 in the Hash Table
+        table.find("660");
+        table.displayTheStack();
+        System.out.println("The number of collisions is : " + table.occupied);
+    }
 }
+
+
