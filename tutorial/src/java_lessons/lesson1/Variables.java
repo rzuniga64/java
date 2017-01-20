bb0ad3b7512234ec8a531e06f8efb7f4d3a9baac
Raul Zuniga <rzuniga64@gmail.com>
2017-01-20 16:16:58 -0600

    Update.

--- tutorial/src/java_lessons/lesson1/Variables.java
+++ tutorial/src/java_lessons/lesson1/Variables.java
@@ -5,11 +5,19 @@ package java_lessons.lesson1;
  *  Always has a type.
  *  Data stored in a variable is called a value.
  *  Variable name must start with a letter.
- *  May contain letters, numbers, and teh underscore character.
+ *  May contain letters, numbers, and the underscore character.
  *  Variables should begin with lowercase letters.
  */
-public class Variables {
-    public static void main(String[] args)
+public final class Variables {
+
+    /** Constructor. */
+    private Variables() { }
+
+    /**
+     * Unit tests.
+     * @param args the arguments
+     */
+    public static void main(final String[] args)
     {
         // Integral types
         int int1       = 1;    // -2**31 - 2**31
@@ -18,9 +26,10 @@ public class Variables {
         double double1 = 1.2d; // 1023 digits after decimal
         byte byte1     = 1;    // -128 - 127
         short short1   = 1;    // -2**15 - 2**15
-        char letter     = 'a';  // alphabetic characters stored as numeric value (ASCII)
+        char letter     = 'a';  // alphabetic chars stored as ASCII
 
-        int value = int1 + (int) long1 + (int) float1 + (int)double1 + byte1 + short1;
+        int value = int1 + (int) long1 + (int) float1 + (int) double1 + byte1
+                + short1;
 
         // Non-numeric data types
         String string1 = "Hello"; // String is a class
