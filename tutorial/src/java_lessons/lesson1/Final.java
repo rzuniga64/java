bc63889bbd00be62dc0d0f03671deb6cd78df37a
Raul Zuniga <rzuniga64@gmail.com>
2017-01-20 16:20:23 -0600

    Update.

--- tutorial/src/java_lessons/lesson1/Final.java
+++ tutorial/src/java_lessons/lesson1/Final.java
@@ -1,9 +1,18 @@
 package java_lessons.lesson1;
 
-public class Final
-{
-   public static void main(String[] args)
-   {
+/**
+ * Class Final.
+ */
+public final class Final {
+
+   /** Constructor. */
+   private Final() { }
+
+   /**
+    * Unit tests.
+    * @param args the arguments.
+    */
+   public static void main(final String[] args) {
       final double pi = 3.14159;
       double radius = 2.0;
       double area = pi * (radius * radius);
