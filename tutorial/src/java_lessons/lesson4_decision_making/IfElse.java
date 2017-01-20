f9a0b149ee81122343644ca89337728958214ea2
Raul Zuniga <rzuniga64@gmail.com>
2017-01-20 17:53:54 -0600

    Update.

--- tutorial/src/java_lessons/lesson4_decision_making/IfElse.java
+++ tutorial/src/java_lessons/lesson4_decision_making/IfElse.java
@@ -2,31 +2,35 @@ package java_lessons.lesson4_decision_making;
 
 import java.util.Scanner;
 
-/*
-    if (relational expression)
-    {
-       set of statements;
-    }
-    else
-    {
-       set of statements;
-    }
+/**
+ *  IfElse class.
+ *  if (relational expression)
+ *      set of statements;
+ *  else
+ *      set of statements;
 */
+public final class IfElse {
 
-public class IfElse
-{
-   public static void main(String[] args)
-   {
-      int hoursWorked;
+   /** Constructr. */
+   private IfElse() { }
+
+   /**
+    * Unit tests.
+    * @param args the arguments.
+    */
+   public static void main(final String[] args) {
+
+       final double rate = 25.00;
+       int hoursWorked;
+       double grossPay;
+
       Scanner input = new Scanner(System.in);
       System.out.print("Enter hours worked: ");
       hoursWorked = input.nextInt();
-      double rate = 25.00;
-      double grossPay;
+
       if (hoursWorked > 40) {
          grossPay = (40 * rate) + ((hoursWorked - 40) * (rate * 1.5));
-      }
-      else {
+      } else {
          grossPay = hoursWorked * rate;
       }
       System.out.println("The gross pay is " + grossPay);
