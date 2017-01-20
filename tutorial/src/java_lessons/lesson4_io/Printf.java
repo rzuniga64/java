c68b52f4430b6fbf4a92dee668394019fcf1812e
Raul Zuniga <rzuniga64@gmail.com>
2017-01-20 17:08:22 -0600

    Update.

--- tutorial/src/java_lessons/lesson4_io/Printf.java
+++ tutorial/src/java_lessons/lesson4_io/Printf.java
@@ -15,6 +15,7 @@ public final class Printf {
    public static void main(final String[] args) {
 
        final double pi = 3.14159263;
-       System.out.printf("pi = %.2f", pi);
+       System.out.printf("pi = %.2f\n", pi);
+       System.out.printf("pi = %10.5f", pi);
    }
 }
