e6c985fb1026f0e2b9a84700aaab364051bbc2ba
Raul Zuniga <rzuniga64@gmail.com>
2017-01-20 17:49:36 -0600

    Update.

--- tutorial/src/java_lessons/lesson4_decision_making/LogicalOperators.java
+++ tutorial/src/java_lessons/lesson4_decision_making/LogicalOperators.java
@@ -1,16 +1,24 @@
 package java_lessons.lesson4_decision_making;
 
-public class LogicalOperators
-{
-   public static void main(String[] args)
-   {
-      // && - And logical operator
-      // || - Or logical operator
+/**
+ * LogicalOperators class.
+ * && And logical operator
+ * || Or logical operator
+ */
+public final class LogicalOperators {
 
+   /** Constructor. */
+   private LogicalOperators() { }
+
+   /**
+    * Unit tests.
+    * @param args the arguments.
+    */
+   public static void main(final String[] args) {
       int hoursWorked = 39;
       int salary = 47000;
       System.out.println((hoursWorked > 40) && (salary <= 50000));
-      String password = "GUEST";
-      System.out.println((password.equals("guest")) || (password.equals("GUEST")));
+      String passwd = "GUEST";
+      System.out.println((passwd.equals("guest")) || (passwd.equals("GUEST")));
    }
 }
