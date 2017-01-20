629b63a4b9e0e7fa1f37e9935ee8be63ad19b287
Raul Zuniga <rzuniga64@gmail.com>
2017-01-20 17:18:19 -0600

    Update.

--- tutorial/src/java_lessons/lesson4_io/Scanner1.java
+++ tutorial/src/java_lessons/lesson4_io/Scanner1.java
@@ -2,18 +2,31 @@ package java_lessons.lesson4_io;
 
 import java.util.Scanner;
 
-public class Scanner1
-{
-   public static void main(String[] args)
-   {
-       Scanner input = new Scanner(System.in);
+/**
+ *  Scanner 1 class.
+ *  Console represented by System.out.
+ *  Keyboard represented by System.in.
+ */
+public final class Scanner1 {
+
+    /** Constructor. */
+    private Scanner1() { }
+
+    /**
+     * Unit tests.
+     * @param args the arguments.
+     */
+   public static void main(final String[] args) {
+
        double number, number1, sum;
+       Scanner input = new Scanner(System.in);
+
        System.out.print("Enter the first number: ");
        number = input.nextDouble();
        System.out.print("Enter the second number: ");
        number1 = input.nextDouble();
+
        sum = number + number1;
-       System.out.println("The sum is " + sum);
-       
+       System.out.printf("Sum = %.2f\n", sum);
    }
 }
