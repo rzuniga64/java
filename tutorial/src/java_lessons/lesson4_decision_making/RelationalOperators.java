94f60e684d4b6af7ce5e9b6b20e6e0aed85d3907
Raul Zuniga <rzuniga64@gmail.com>
2017-01-20 17:36:44 -0600

    Update.

--- tutorial/src/java_lessons/lesson4_decision_making/RelationalOperators.java
+++ tutorial/src/java_lessons/lesson4_decision_making/RelationalOperators.java
@@ -1,17 +1,25 @@
 package java_lessons.lesson4_decision_making;
 
-public class RelationalOperators
-{
-   public static void main(String[] args)
-   {
-      /*
-         == - equality
-         > - greater than
-         >= - greater than or equal to
-         < - less than
-         <= - less than or equal to
-         != - not equal to
-      */
+/**
+ *  RelationalOperators class.
+ *  ==  equality
+ *  >   greater than
+ *  >=  greater than or equal to
+ *  <   less than
+ *  <=  less than or equal to
+ *  !=  not equal to
+ */
+public final class RelationalOperators {
+
+    /** Constructor. */
+    private RelationalOperators() { }
+
+    /**
+     * Unit tests.
+     * @param args the arguments.
+     */
+   public static void main(final String[] args) {
+
       System.out.println(!("hello".equals("Hello")));
       System.out.println(2 <= 3);
       String password = "password";
