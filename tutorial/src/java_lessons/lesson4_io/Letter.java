7bff8046b69d6a892dc6c72aba8462637182fc7d
Raul Zuniga <rzuniga64@gmail.com>
2017-01-20 17:02:14 -0600

    Update.

--- tutorial/src/java_lessons/lesson4_io/Letter.java
+++ tutorial/src/java_lessons/lesson4_io/Letter.java
@@ -2,22 +2,36 @@ package java_lessons.lesson4_io;
 
 import java.util.Scanner;
 
-public class Letter
-{
-   public static void main(String[] args)
-   {
-      String name, present, yourName;
+/**
+ * Letter class.
+ */
+public final class Letter {
+
+    /** Constructor. */
+    private Letter() { }
+    /**
+     * Unit tests.
+     * @param args the arguemnts
+     */
+   public static void main(final String[] args) {
+
+       String name, present, yourName;
       int age;
       Scanner inputString = new Scanner(System.in);
       Scanner inputNumber = new Scanner(System.in);
+
       System.out.print("What is the name of the gift giver? ");
       name = inputString.nextLine();
+
       System.out.print("What is the present they gave you? ");
       present = inputString.nextLine();
+
       System.out.print("How old were you on your birthday? ");
       age = inputNumber.nextInt();
+
       System.out.print("What is your name? ");
       yourName = inputString.nextLine();
+
       System.out.println();
       System.out.println();
       System.out.println("Dear " + name + ", ");
@@ -26,7 +40,7 @@ public class Letter
       System.out.println("I really like it. I can't believe ");
       System.out.print("I'm already " + age + " years old, but ");
       System.out.println("it doesn't feel that much different than ");
-      System.out.println("being " + (age-1) + ". ");
+      System.out.println("being " + (age - 1) + ". ");
       System.out.println();
       System.out.println("Sincerely, ");
       System.out.println();
