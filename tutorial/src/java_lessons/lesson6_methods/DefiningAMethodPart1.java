5697da6f9c2ea0470cb27bd0ab9005bf311978a5
Raul Zuniga <rzuniga64@gmail.com>
2017-01-20 19:55:55 -0600

    Update.

--- tutorial/src/java_lessons/lesson6_methods/DefiningAMethodPart1.java
+++ tutorial/src/java_lessons/lesson6_methods/DefiningAMethodPart1.java
@@ -1,19 +1,33 @@
 package java_lessons.lesson6_methods;
 
-// A method is a set of instructions that performs a named operation.
-// It takes data as input and may return data as output.
+/**
+ * DefiningAMethodPart1 class.
+ *
+ * A method is a set of instructions that performs a named operation.
+ * It takes data as input and may return data as output.
+ */
+public final class DefiningAMethodPart1 {
 
-public class DefiningAMethodPart1
-{
-   public static void main(String[] args) 
-   {
-      int num = 12;
-      System.out.println(num + " squared equals " + square(num));     
-   }
+    /** Constructor. */
+    private DefiningAMethodPart1() { }
 
-    // Static means an independent method.
-    // Parameters must match in data type and number.
-   static int square(int number) {
+    /**
+     *  Static means an independent method.
+     *  Parameters must match in data
+     *  type and number.
+     * @param number the number to square
+     * @return the square of the number
+     */
+   static int square(final int number) {
       return number * number;
    }
+
+    /**
+     * Unit tests.
+     * @param args the arguments.
+     */
+    public static void main(final String[] args) {
+        int num = 12;
+        System.out.println(num + " squared equals " + square(num));
+    }
 }
