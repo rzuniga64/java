20d86535a6d976f998dc34af1475275283e56e4c
Raul Zuniga <rzuniga64@gmail.com>
2017-01-20 20:11:46 -0600

    Update.

--- tutorial/src/java_lessons/lesson6_methods/MultipleParameters.java
+++ tutorial/src/java_lessons/lesson6_methods/MultipleParameters.java
@@ -2,39 +2,66 @@ package java_lessons.lesson6_methods;
 
 import java.util.Scanner;
 
-public class MultipleParameters
-{
-   public static void main(String[] arts) 
-   {
-      Scanner inputTemp = new Scanner(System.in);
-      Scanner input = new Scanner(System.in);
-      double temperature;
-      String type;
-      System.out.print("Enter a temperature to convert: ");
-      temperature = inputTemp.nextDouble();
-      System.out.print("Enter type to convert to (C or F): ");
-      type = input.next();
-      System.out.println("The converted temperature is " + 
-                         convert(temperature, type));
-   }
-   
-   static double ftoc(double temp) {
+/**
+ * MultipleParameters class.
+ */
+public final class MultipleParameters {
+
+   /** Constructor. */
+   private MultipleParameters() { }
+
+   /**
+    * Converts fahrenheit to celsius.
+    * @param temp the temperature
+    * @return the temperatur in fahrenheit
+    */
+    static double ftoc(final double temp) {
       double celsius;
       celsius = (temp - 32.0) * (5.0 / 9.0);
       return celsius;
    }
 
-   static double ctof(double temp) {
+   /**
+    * Converts celsius to fahrenheit.
+    * @param temp the temperature
+    * @return the temperatur in fahrenheit
+    */
+   static double ctof(final double temp) {
       double fahrenheit;
       fahrenheit = temp * (9.0 / 5.0) + 32.0;
       return fahrenheit;
    }
 
-   static double convert(double temp, String type) {
+    /**
+     * Convert method.
+     * @param temp the temperature
+     * @param type the type
+     * @return the temperature
+     */
+   static double convert(final double temp, final String type) {
       if (type.equals("C"))
          return (temp - 32.0) * (5.0 / 9.0);
       else
          return temp * (9.0 / 5.0) + 32.0;
    }
 
+    /**
+     * Unit tests.
+     * @param args the arguments.
+     */
+    public static void main(final String[] args) {
+
+        double temperature;
+        String type;
+
+        Scanner inputTemp = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a temperature to convert: ");
+        temperature = inputTemp.nextDouble();
+        System.out.print("Enter type to convert to (C or F): ");
+        type = input.next();
+        System.out.println("The converted temperature is "
+                + convert(temperature, type));
+    }
 }
