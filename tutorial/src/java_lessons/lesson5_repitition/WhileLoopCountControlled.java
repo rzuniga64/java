4c82ebd6b8ee96cab601e96bcc4708d369e69e4c
Raul Zuniga <rzuniga64@gmail.com>
2017-01-20 19:16:16 -0600

    Update.

--- tutorial/src/java_lessons/lesson5_repitition/WhileLoopCountControlled.java
+++ tutorial/src/java_lessons/lesson5_repitition/WhileLoopCountControlled.java
@@ -1,20 +1,29 @@
 package java_lessons.lesson5_repitition;
 
-public class WhileLoopCountControlled
-{
-   public static void main(String[] args)
-   {
-      // balance, rate, number of years
-      double balance = 5000;
-      double rate = 1.02;
-      // create a loop control variable
-      int year = 1;
-      while (year <= 10) {  		//test loop control variable
-         balance = balance * rate; 	// balance *= rate;
-         System.out.printf("After year " + year + ", the balance is %.2f\n", balance);
-         ++year;					// modify loop control variable 
-      }
-      System.out.printf("After 10 years the balance is %.2f.\n", balance);
-    
-   }
+/**
+ * WhileLoopCountControlled class.
+ */
+public final class WhileLoopCountControlled {
+
+    /** Constructor. */
+    private LoopingCalculator() { }
+
+    /**
+     * Unit tests.
+     * @param args the arguments.
+     */
+    public static void main(final String[] args) {
+
+        // balance, rate, number of year
+        double balance = 5000;
+        double rate = 1.02;
+        // create a loop control variable
+        int year = 1;
+        while (year <= 10) {  		//test loop control variable
+            balance = balance * rate; 	// balance *= rate;
+            System.out.printf("After year " + year + ", the balance is %.2f\n", balance);
+            ++year;					// modify loop control variable
+        }
+        System.out.printf("After 10 years the balance is %.2f.\n", balance);
+    }
 }
