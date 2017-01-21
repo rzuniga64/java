f153778443731155dbeeaec6ae90a3c439ed6562
Raul Zuniga <rzuniga64@gmail.com>
2017-01-21 05:14:34 -0600

    Update.

--- tutorial/src/java_lessons/lesson11_polymorphism/Polymorphism.java
+++ tutorial/src/java_lessons/lesson11_polymorphism/Polymorphism.java
@@ -1,15 +1,21 @@
 package java_lessons.lesson11_polymorphism;
- 
-// Test program to show how draw differs for 
-// a Circle object and a Rectangle object
-// using polymorphism.
 
-public class Polymorphism
-{
-   public static void main(String[] args) 
-   {
-      Circle c1 = new Circle(10,20,5);
-      Rectangle r1 = new Rectangle(50,100,10,20);
+/**
+ * Polymorphism class.
+ * Test program to show how draw differs for a Circle object and a Rectangle
+ * object using polymorphism.
+ */
+public final class Polymorphism {
+
+   /** Constructor. */
+   private Polymorphism() { }
+   /**
+    * Unit tests.
+    * @param args the arguments.
+    */
+   public static void main(final String[] args) {
+      Circle c1 = new Circle(10, 20, 5);
+      Rectangle r1 = new Rectangle(50, 100, 10, 20);
       c1.draw();
       System.out.println();
       r1.draw();
