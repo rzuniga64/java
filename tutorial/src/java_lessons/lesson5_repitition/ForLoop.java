efeb3d24e070064f991c8be288706f5932d1e564
Raul Zuniga <rzuniga64@gmail.com>
2017-01-20 18:37:04 -0600

    Update.

--- tutorial/src/java_lessons/lesson5_repitition/ForLoop.java
+++ tutorial/src/java_lessons/lesson5_repitition/ForLoop.java
@@ -1,21 +1,36 @@
 package java_lessons.lesson5_repitition;
 
 /**
- * lcv - loop control variable
+ * ForLoop class.
  *
  * for(initialize lcv; rel test lcv; modify lcv) {
- * statements;
+ *    statements;
  * }
+ *
  */
+public final class ForLoop {
 
-public class ForLoop
-{
-   public static void main(String[] args)
-   {
+    /** Constructor. */
+    private ForLoop() { }
+
+    /**
+     * Unit tests.
+     * @param args the arguments.
+     */
+   public static void main(final String[] args) {
+
       int sum = 0;
-      for(int i = 1; i <= 10; ++i) {
+      double balance = 5000;
+      double rate = 1.02;
+
+      for (int i = 1; i <= 10; ++i) {
          sum += i;
       }
       System.out.println("The sum of 1 to 10 is " + sum);
+
+      for (int i = 1; i <= 10; ++i) {
+          balance *= rate;
+       }
+       System.out.printf("The balance after 10 years is %.2f", balance);
    }
 }
