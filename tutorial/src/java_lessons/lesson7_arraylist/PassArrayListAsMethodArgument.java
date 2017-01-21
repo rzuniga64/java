3b5f7e82cc8c6c06477ba2f5d4f8bfb36b5c3a63
Raul Zuniga <rzuniga64@gmail.com>
2017-01-20 21:12:01 -0600

    Update.

--- tutorial/src/java_lessons/lesson7_arraylist/PassArrayListAsMethodArgument.java
+++ tutorial/src/java_lessons/lesson7_arraylist/PassArrayListAsMethodArgument.java
@@ -2,29 +2,53 @@ package java_lessons.lesson7_arraylist;
 
 import java.util.ArrayList;
 
-public class PassArrayListAsMethodArgument
-{
-   public static void main(String[] args) 
-   {
-      ArrayList<Integer> numbers = new ArrayList<Integer>();
-      for(int i = 1; i < 11; ++i)
-         numbers.add(i);
-      display(numbers);
-      change(numbers, 5);
-      display(numbers);
-   }
+/**
+ * PassArrayListAsMethodArgument class.
+ */
+public final class PassArrayListAsMethodArgument {
 
-   static void display(ArrayList arr) {
-      for(int i = 0; i < arr.size(); ++i)
-         System.out.print(arr.get(i) + " ");
+    /**
+     * Constructor.
+     */
+    private PassArrayListAsMethodArgument() { }
+
+   /**
+    * Display the contents of an array.
+    * @param arr the array
+    */
+   static void display(final ArrayList arr) {
+      for (Object item : arr) {
+         System.out.print(item + " ");
+      }
       System.out.println();
    }
 
-   static void change(ArrayList<Integer> arr, int amount) {
-      int value;
-      for(int i = 0; i < arr.size(); ++i) {
-         value = arr.get(i);
-         arr.set(i, value + amount);
-      }
+   /**
+    * Change the contents of an array list.
+    * @param arr the array
+    * @param amount the amount
+    */
+   static void change(final ArrayList<Integer> arr, final int amount) {
+       int value;
+       for (int i = 0; i < arr.size(); ++i) {
+           value = arr.get(i);
+           arr.set(i, value + amount);
+       }
    }
+
+    /**
+     * Unit tests.
+     *
+     * @param args the arguments.
+     */
+    public static void main(final String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i < 11; ++i) {
+            numbers.add(i);
+        }
+        display(numbers);
+        change(numbers, 5);
+        display(numbers);
+    }
 }
