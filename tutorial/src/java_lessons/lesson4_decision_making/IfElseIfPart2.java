3f6e181aa879d6c88c63285ee943061f36e27d51
Raul Zuniga <rzuniga64@gmail.com>
2017-01-20 18:13:14 -0600

    Update.

--- tutorial/src/java_lessons/lesson4_decision_making/IfElseIfPart2.java
+++ tutorial/src/java_lessons/lesson4_decision_making/IfElseIfPart2.java
@@ -3,33 +3,47 @@ package java_lessons.lesson4_decision_making;
 import java.util.Scanner;
 
 /**
- * Temperature	Activity
- * > 85		Swimming
- * 70-85		Tennis
- * 32-69		Golf
- * 0 - 31		Dancing
- * Below 0		Sit by the fire
+ *  IfElseIfPart2 class.
+ *
+ *  Temperature Activity
+ *  > 85        Swimming
+ *  70-85       Tennis
+ *  32-69       Golf
+ *  0 - 31      Dancing
+ *  Below 0     Sit by the fire
 */
+public final class IfElseIfPart2 {
 
-public class IfElseIfPart2
-{
-   public static void main(String[] args) 
-   {
-      String message = "The recommended activity is ";
-      int temp;
-      Scanner input = new Scanner(System.in);
-      System.out.print("Enter the temperature: ");
-      temp = input.nextInt();
-      if (temp > 85)
-         message = message + "swimming.";
-      else if (temp >= 70)
-         message = message + "tennis.";
-      else if (temp >= 32)
-         message = message + "golf.";
-      else if (temp >= 0)
-         message = message + "dancing.";
-      else
-         message = message + "sitting by the fire.";
-      System.out.println(message);
+    /**
+     * Constructor.
+     */
+    private IfElseIfPart2() {}
+
+    /**
+     * Unit tests.
+     *
+     * @param args the arguments.
+     */
+    public static void main(final String[] args) {
+
+        int temp;
+        String message = "The recommended activity is ";
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the temperature: ");
+        temp = input.nextInt();
+        if (temp > 85) {
+            message = message + "swimming.";
+        } else if (temp >= 70) {
+            message = message + "tennis.";
+        } else if (temp >= 32) {
+            message = message + "golf.";
+        } else if (temp >= 0) {
+            message = message + "dancing.";
+        } else {
+            message = message + "sitting by the fire.";
+        }
+        System.out.println(message);
    }
 }
+
