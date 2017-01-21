51735063094fa3913cfa5a082b8943d44d149caa
Raul Zuniga <rzuniga64@gmail.com>
2017-01-20 19:08:31 -0600

    Update.

--- tutorial/src/java_lessons/lesson5_repitition/LoopingQA.java
+++ tutorial/src/java_lessons/lesson5_repitition/LoopingQA.java
@@ -2,28 +2,40 @@ package java_lessons.lesson5_repitition;
 
 import java.util.Scanner;
 
-public class LoopingQA
-{
-   public static void main(String[] args)
-   {
+/**
+ * LoopingQA class.
+ */
+public final class LoopingQA {
+
+    /** Constructor. */
+    private LoopingQA() { }
+
+    /**
+     * Unit tests.
+     * @param args the arguments.
+     */
+   public static void main(final String[] args) {
+
       String answer = "Watson";
       String response;
       int tries = 0;
+
       Scanner input = new Scanner(System.in);
+
       while (tries <= 3) {
-         System.out.print("Enter the name of the computer that played on Jeopardy? ");
-         response = input.nextLine();
-         ++tries;
-         if (response.equals(answer)) {
-            System.out.println("That's right!");
-            break;
-         } else if (tries == 3) {
-            System.out.println("Sorry. The answer is Watson.");
-            break;
-         } else {
-            System.out.println("Sorry. Try again.");
-         }
+          System.out.print("Enter the name of the computer that played on "
+                        + "Jeopardy? ");
+          response = input.nextLine();
+          ++tries;
+          if (response.equals(answer)) {
+              System.out.println("That's right!");
+             break;
+          } else if (tries == 3) {
+             System.out.println("Sorry. The answer is Watson.");
+             break;
+          } else {
+             System.out.println("Sorry. Try again.");
+          }
       }
    }
 }
-         
