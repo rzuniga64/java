3b0d21876be1c2038d2a1e6944ea707516e05708
Raul Zuniga <rzuniga64@gmail.com>
2017-01-20 18:01:16 -0600

    Update.

--- tutorial/src/java_lessons/lesson4_decision_making/NestedIfElse.java
+++ tutorial/src/java_lessons/lesson4_decision_making/NestedIfElse.java
@@ -2,42 +2,45 @@ package java_lessons.lesson4_decision_making;
 
 import java.util.Scanner;
 
-/*
-    if (rel exp) {
-       if (rel exp) {
-          if (rel exp) {
-             statement;
-          }
-          else {
-             statement;
-          }
-       }
-    }
-*/
+/**
+ *  IfElse class.
+ *  if (rel expression)
+ *    if (rel expression)
+ *       if (rel exp)
+ *          set of statements;
+ *       else
+ *          set of statements;
+ */
+public final class NestedIfElse {
 
-public class NestedIfElse
-{
-   public static void main(String[] args)
-   {
-      int grade;
-      String letterGrade;
-      Scanner input = new Scanner(System.in);
-      System.out.print("Enter a grade: ");
-      grade = input.nextInt();
-      if (grade >= 90) { 
-         if (grade >= 95) {
-            letterGrade = "A+";
-         }
-         if (grade >= 93) {
-            letterGrade = "A";
-         } else
-            letterGrade = "A-";
-            
-         System.out.println("Your letter grade is " + letterGrade);
-      } else 
-         System.out.println("Sorry, you didn't make an A.");
-      
+   /** Constructor. */
+   private NestedIfElse() { }
+
+   /**
+    * Unit tests.
+    * @param args the arguments.
+    */
+   public static void main(final String[] args) {
+
+        int grade;
+        String letterGrade;
+        System.out.print("Enter a grade: ");
+        Scanner input = new Scanner(System.in);
+
+        grade = input.nextInt();
+
+        if (grade >= 90) {
+            if (grade >= 95) {
+                letterGrade = "A+";
+            }
+            if (grade >= 93) {
+                letterGrade = "A";
+            } else {
+                letterGrade = "A-";
+            }
+            System.out.println("Your letter grade is " + letterGrade);
+        } else {
+            System.out.println("Sorry, you didn't make an A.");
+        }
    }
 }
-
-      
