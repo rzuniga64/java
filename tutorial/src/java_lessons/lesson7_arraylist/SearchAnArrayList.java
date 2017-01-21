4c47bcc436534cb9b5891a09e91100ce442572b2
Raul Zuniga <rzuniga64@gmail.com>
2017-01-20 21:31:48 -0600

    Update.

--- tutorial/src/java_lessons/lesson7_arraylist/SearchAnArrayList.java
+++ tutorial/src/java_lessons/lesson7_arraylist/SearchAnArrayList.java
@@ -3,23 +3,41 @@ package java_lessons.lesson7_arraylist;
 import java.util.Random;
 import java.util.ArrayList;
 
-public class SearchAnArrayList
-{
-   public static void main(String[] args)
-   {
-      Random rand = new Random(System.currentTimeMillis());
-      ArrayList<Integer> numbers = new ArrayList<Integer>();
-      for(int i = 0; i < 100; ++i)
-         numbers.add(rand.nextInt(101));
+/**
+ * SearchArrayList class.
+ */
+public final class SearchAnArrayList {
 
-      /*if (numbers.contains(100))
-         System.out.println("100 was found at position " + numbers.indexOf(100));
-      else
-         System.out.println("100 was not found in numbers.");*/
-      int min = 0;
-      for(int i = 1; i < numbers.size(); ++i) 
-         if (numbers.get(i) < numbers.get(min))
-            min = i;
-      System.out.println("The minimum value in numbers is " + numbers.get(min));
-   }
+    /**
+     * Constructor.
+     */
+    private SearchAnArrayList() { }
+
+    /**
+     * Unit tests.
+     * @param args the arguments.
+     */
+    public static void main(final String[] args) {
+
+        Random rand = new Random(System.currentTimeMillis());
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        for (int i = 0; i < 100; ++i) {
+            numbers.add(rand.nextInt(101));
+        }
+
+        /*if (numbers.contains(100))
+            System.out.println("100 was found at position "
+                                    + numbers.indexOf(100));
+        else
+            System.out.println("100 was not found in numbers.");*/
+
+        int min = 0;
+        for (int i = 1; i < numbers.size(); ++i) {
+            if (numbers.get(i) < numbers.get(min)) {
+                min = i;
+            }
+        }
+        System.out.println("The min value in numbers is " + numbers.get(min));
+    }
 }
