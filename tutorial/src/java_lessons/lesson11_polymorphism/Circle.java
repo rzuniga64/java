3acba0737cc9f33fdbb0790dbbdfb5bd3d689fc4
Raul Zuniga <rzuniga64@gmail.com>
2017-01-21 04:51:51 -0600

    Update.

--- tutorial/src/java_lessons/lesson11_polymorphism/Circle.java
+++ tutorial/src/java_lessons/lesson11_polymorphism/Circle.java
@@ -1,18 +1,46 @@
 package java_lessons.lesson11_polymorphism;
 
-public class Circle extends Shape {
+/**
+ * Circle class.
+ */
+public final class Circle extends Shape {
+
+    /** The radius of the circle. */
     private int radius;
-    
-    Circle(int xcoor, int ycoor, int r) {
+
+    /**
+     * Constructor.
+     * @param xcoor x-ccordinate
+     * @param ycoor y-coordinate
+     * @param r the radius
+     */
+    private Circle(final int xcoor, final int ycoor, final int r) {
         super(xcoor, ycoor);
         setRadius(r);
     }
-    
-    public int getRadius() { return radius; }
-    public void setRadius(int r) { radius = r; }
+
+    /**
+     * Get the radius.
+     * @return the radius
+     */
+    public int getRadius() {
+
+        return radius;
+    }
+
+    /**
+     * Set the radius.
+     * @param r the radius
+     */
+    public void setRadius(final int r) {
+
+        radius = r;
+    }
+
     @Override
+    /** Draw method. */
     public void draw() {
-        System.out.println("Drawing a circle at x: " + getX() + ", y: " + getY() +
-                            " with radius of: " + getRadius());
+        System.out.println("Drawing a circle at x: " + getX() + ", y: " + getY()
+                + " with radius of: " + getRadius());
     }
 }
