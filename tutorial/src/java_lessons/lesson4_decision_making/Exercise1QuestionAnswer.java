9ac7000bd3532eb71c916f1beeb3d97af224c631
Raul Zuniga <rzuniga64@gmail.com>
2017-01-20 18:24:29 -0600

    Update.

--- tutorial/src/java_lessons/lesson4_decision_making/Exercise1QuestionAnswer.java
+++ tutorial/src/java_lessons/lesson4_decision_making/Exercise1QuestionAnswer.java
@@ -2,30 +2,43 @@ package java_lessons.lesson4_decision_making;
 
 import java.util.Scanner;
 
-public class Exercise1QuestionAnswer
+/**
+ * Exercise1QuestionAnswer class.
+ */
+public final class Exercise1QuestionAnswer
 {
-   public static void main(String[] args) 
-   {
-      Scanner input = new Scanner(System.in);
-      String answer = "Watson";
-      String response;
-      System.out.println("Let's play a guessing game. You get three tries.");
-      System.out.print("What was the name of the computer that played on Jeopardy? ");
-      response = input.nextLine();
-      if (response.equals(answer))
-         System.out.println("That's right!");
-      else {
-         System.out.print("Sorry. Guess again: ");
-         response = input.nextLine();
-         if (response.equals(answer))
+   /** Constructor. */
+   private Exercise1QuestionAnswer() { }
+
+   /**
+    * Unit tests.
+    * @param args the arguments.
+    */
+   public static void main(final String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String answer = "Watson";
+        String response;
+        System.out.println("Let's play a guessing game. You get three tries.");
+        System.out.print("What was the name of the computer that played on "
+                        + "Jeopardy? ");
+        response = input.nextLine();
+
+        if (response.equals(answer)) {
             System.out.println("That's right!");
-         else {
-            System.out.print("Still not right. One more guess: ");
+        } else {
+            System.out.print("Sorry. Guess again: ");
             response = input.nextLine();
-            if (response.equals(answer))
-               System.out.println("That's right!");
-            else
-               System.out.println("Wrong again. The answer is Watson.");
+            if (response.equals(answer)) {
+                System.out.println("That's right!");
+            } else {
+                System.out.print("Still not right. One more guess: ");
+                response = input.nextLine();
+                if (response.equals(answer)) {
+                    System.out.println("That's right!");
+                } else {
+                    System.out.println("Wrong again. The answer is Watson.");
+                }
          }
       }
    }
