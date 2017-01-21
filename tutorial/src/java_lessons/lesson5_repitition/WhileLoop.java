b5bc1125636d6242e0e6bb6522fb2856f21baf67
Raul Zuniga <rzuniga64@gmail.com>
2017-01-20 18:31:33 -0600

    Update.

--- tutorial/src/java_lessons/lesson5_repitition/WhileLoop.java
+++ tutorial/src/java_lessons/lesson5_repitition/WhileLoop.java
@@ -1,15 +1,23 @@
 package java_lessons.lesson5_repitition;
 
 /**
+ * WhileLoop class.
+ *
  * while (relational expression) {
- * loop body
+ *    loop body
  * }
+ *
  */
+public final class WhileLoop  {
 
-public class WhileLoop
-{
-   public static void main(String[] args)
-   {
+    /** Constructor. */
+    private WhileLoop() { }
+
+    /**
+     * Unit tests.
+     * @param args the arguments.
+     */
+   public static void main(final String[] args) {
       int number = 1;
       while (number <= 10) {
          System.out.println(number);
