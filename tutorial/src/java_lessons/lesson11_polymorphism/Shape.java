343c3ae4c32bad664cfa3f3554deb43e7dc2b798
Raul Zuniga <rzuniga64@gmail.com>
2017-01-21 04:41:45 -0600

    Update.

--- tutorial/src/java_lessons/lesson11_polymorphism/Shape.java
+++ tutorial/src/java_lessons/lesson11_polymorphism/Shape.java
@@ -1,58 +1,97 @@
 package java_lessons.lesson11_polymorphism;
 
-/* An abstract class that can be inherited from 
-   but can not be instantiated directly.  Used as
-   a basis to create other classes. For Example,
-   we don't normally want to create a Shape we want
-   to implement a type of Shape(Circle, Rectangle,etc).
-
-   To make a class a superclass while not allowing a 
-   user to instantaiate it is to make it abstract.
+/**
+ * Shape class.
+ *
+ * An abstract class that can be inherited from but can not be instantiated
+ * directly.  Used as a basis to create other classes. For Example, we don't
+ * normally want to create a Shape we want to implement a type of Shape
+ * (Circle, Rectangle,etc).
+ *
+ * To make a class a superclass while not allowing a
+ * user to instantaiate it is to make it abstract.
 */
 
-public abstract class Shape
-{
+public abstract class Shape {
+   /** x ooordinate. */
    private int x;
+   /** y coordinate. */
    private int y;
 
-   Shape(int xcor, int ycor) {
-      x = xcor;
-      y = ycor;
+   /**
+    * Constructor.
+    * @param xcoor x-coordinate
+    * @param ycoor y-coordinate
+    */
+   Shape(final int xcoor, final int ycoor) {
+      x = xcoor;
+      y = ycoor;
    }
 
+   /**
+    * Get the x-coordinate.
+    * @return the x-coordinate
+    */
    public int getX() {
+
       return x;
    }
-  
+
+   /**
+    * Get the y-coordinate.
+    * @return the y-coordinate
+    */
    public int getY() {
+
       return y;
    }
 
-   public void setX(int xcor) {
-      x = xcor;
+   /**
+    * Set the x-coordinate.
+    * @param xcoor x-coordinate
+    */
+   public void setX(final int xcoor) {
+
+      x = xcoor;
    }
 
-   public void setY(int ycor) {
-      y = ycor;
+   /**
+    * Set the y-coordinate.
+    * @param ycoor y-coordinate
+    */
+   public void setY(final int ycoor) {
+
+      y = ycoor;
    }
 
-   public void set(int xcor, int ycor) {
-      setX(xcor);
-      setY(ycor);
+   /**
+    * Set the x-coordinate and y-coordinate.
+    * @param xcoor x-coordinate
+    * @param ycoor y-coordinate
+    */
+   public void set(final int xcoor, final int ycoor) {
+      setX(xcoor);
+      setY(ycoor);
    }
 
-   public void move(int x1, int y1) {
+   /**
+    * Moves the x-coordinate and y-coordinate.
+    * @param x1 x-coordinate
+    * @param y1 y-coordinate
+    */
+   public void move(final int x1, final int y1) {
+
       set(getX() + x1, getY() + y1);
    }
 
-   // Abstract method. A class that inherits from Shape1
-   // must implement a draw method but not necessarily 
-   // inherit the Shape1's draw method.  It doesn't make
-   // sense to have a draw method for shape because we 
-   // haven't defined the shape.  So rather than have a 
-   // body that does something meaningless it's better to 
-   // make the method abstract and then implement in a 
-   // subclass for a particular type of shape.
-   public abstract void draw();
+   /**
+    * Abstract method. A class that inherits from Shape1 must implement a
+    * draw method but not necessarily inherit the Shape1's draw method.  It
+    * doesn't make sense to have a draw method for shape because we haven't
+    * defined the shape.  So rather than have a body that does something
+    * meaningless it's better to make the method abstract and then implement
+    * in a subclass for a particular type of shape.
+    */
+    public abstract void draw();
 }
 
