7130af8e852e37cacaa833d262a22f47cc2a6e55
Raul Zuniga <rzuniga64@gmail.com>
2017-01-21 04:18:03 -0600

    Update.

--- tutorial/src/java_lessons/lesson10_inheritance/Shape.java
+++ tutorial/src/java_lessons/lesson10_inheritance/Shape.java
@@ -1,24 +1,78 @@
 package java_lessons.lesson10_inheritance;
 
+/**
+ * Shape class.
+ */
 public class Shape {
+
+    /** x ooordinate. */
     private int x;
+    /** y coordinate. */
     private int y;
-    
-    Shape(int xcoor, int ycoor) {
+
+    /**
+     * Constructor.
+     * @param xcoor x-coordinate
+     * @param ycoor y-coordinate
+     */
+    Shape(final int xcoor, final int ycoor) {
         x = xcoor;
         y = ycoor;
     }
-    
-    public int getX() { return x; }
-    public int getY() { return y; }
-    public void setX( int xcoor) { x = xcoor; }
-    public void setY( int ycoor) { y = ycoor; }
-    public void set( int xcoor, int ycoor) {
+
+    /**
+     * Get the x-coordinate.
+     * @return the x-coordinate
+     */
+    public int getX() {
+
+        return x;
+    }
+
+    /**
+     * Get the y-coordinate.
+     * @return the y-coordinate
+     */
+    public int getY() {
+
+        return y;
+    }
+
+    /**
+     * Set the x-coordinate.
+     * @param xcoor x-coordinate
+     */
+    public void setX(final int xcoor) {
+
+        x = xcoor;
+    }
+
+    /**
+     * Set the y-coordinate.
+     * @param ycoor y-coordinate
+     */
+    public void setY(final int ycoor) {
+
+        y = ycoor;
+    }
+
+    /**
+     * Set the x-coordinate and y-coordinate.
+     * @param xcoor x-coordinate
+     * @param ycoor y-coordinate
+     */
+    public void set(final int xcoor, final int ycoor) {
         setX(xcoor);
         setY(ycoor);
     }
-    
-    public void move(int x1, int y1) {
+
+    /**
+     * Moves the x-coordinate and y-coordinate.
+     * @param x1 x-coordinate
+     * @param y1 y-coordinate
+     */
+    public void move(final int x1, final int y1) {
+
         set(getX() + x1, getY() + y1);
     }
 }
