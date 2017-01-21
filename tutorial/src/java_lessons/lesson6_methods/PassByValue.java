82545f356a77cd62188c23c6c0f511a0ce4f3cb4
Raul Zuniga <rzuniga64@gmail.com>
2017-01-20 20:19:20 -0600

    Update.

--- tutorial/src/java_lessons/lesson6_methods/PassByValue.java
+++ tutorial/src/java_lessons/lesson6_methods/PassByValue.java
@@ -1,18 +1,32 @@
 package java_lessons.lesson6_methods;
 
-public class PassByValue
-{
-   public static void main(String[] args)
-   {
+/**
+ * PassByValue class.
+ */
+public final class PassByValue {
+
+   /** Constructor. */
+   private PassByValue() { }
+
+   /**
+    * Unit tests.
+    * @param args the arguments.
+    */
+   public static void main(final String[] args) {
       int number = 12;
       System.out.println("number squared: " + square(number));
       System.out.println("original number: " + number);
    }
 
-   static int square(int num) {
+   /**
+    * Square a number.
+    * @param num the number
+    * @return the square of a number
+    */
+   private static int square(int num) {
+
       num *= num;
       System.out.println("num in square method: " + num);
       return num;
    }
-
 }
