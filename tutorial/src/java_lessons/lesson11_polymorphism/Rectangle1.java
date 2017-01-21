e53cd09c49fc540ca5682507e6af29239f188782
Raul Zuniga <rzuniga64@gmail.com>
2017-01-21 05:08:06 -0600

    Update.

--- tutorial/src/java_lessons/lesson11_polymorphism/Rectangle1.java
+++ tutorial/src/java_lessons/lesson11_polymorphism/Rectangle1.java
@@ -1,39 +1,119 @@
 package java_lessons.lesson11_polymorphism;
 
-// Develop a class from an interface
-// An interface is another way to build complex
-// hierchies in Java.
-public class Rectangle1 implements Shape1
-{
-   private int x;
-   private int y;
-   private int width;
-   private int height;
+/**
+ * Rectangle1 interface.
+ * Develop a class from an interface
+ * An interface is another way to build complex
+ * hierchies in Java.
+ */
+public class Rectangle1 implements Shape1 {
 
-   Rectangle1(int xcor, int ycor, int w, int h) {
-      x = xcor;
-      y = ycor;
+    /** x ooordinate. */
+    private int x;
+    /** y coordinate. */
+    private int y;
+    /** Width of rectangle. */
+    private int width;
+    /** Height of rectangle. */
+    private int height;
+
+/**
+ * Constructor.
+ * @param xcoor x-coordinate
+ * @param ycoor y-coordinate
+ * @param w width
+ * @param h height
+ */
+    Rectangle1(final int xcoor, final int ycoor, final int w, final int h) {
+      x = xcoor;
+      y = ycoor;
       width = w;
       height = h;
    }
 
-   public int getWidth()  { return width; }
-   public int getHeight() { return height; }
-   public void setWidth(int w) { width = w; }
-   public void setHeight(int h) { height = h; }  
-   
+    /**
+     * Get the width.
+     * @return the width
+     */
+   public int getWidth()  {
+
+       return width;
+   }
+
+    /**
+     * Get the height.
+     * @return the height.
+     */
+   public int getHeight() {
+
+       return height;
+   }
+
+    /**
+     * Set the width.
+     * @param w the width
+     */
+   public void setWidth(final int w) {
+
+       width = w;
+   }
+
+    /**
+     * Set the height.
+     * @param h the height
+     */
+   public void setHeight(final int h) {
+
+       height = h;
+   }
+
    // Must implement the methods listed in the Shape1 interface
    @Override
-   public int getX() {return x;}
+   /**
+    * Get the x-coordinate.
+    * @return the x-coordinate
+    */
+   public int getX() {
+
+       return x;
+   }
+
    @Override
-   public int getY() {return y;}
+   /**
+    * Get the y-coordinate.
+    * @return the y-coordinate
+    */
+   public int getY() {
+
+       return y;
+   }
+
    @Override
-   public void setX(int xcor) {x = xcor;}
+   /**
+    * Set the x-coordinate.
+    * @param xcoor x-coordinate
+    */
+   public void setX(final int xcor) {
+
+       x = xcor;
+   }
+
    @Override
-   public void setY(int ycor) {y = ycor;}
+   /**
+    * Set the y-coordinate.
+    * @param ycoor y-coordinate
+    */
+   public void setY(final int ycor) {
+
+       y = ycor;
+   }
+
    @Override
+   /**
+    * The draw method.
+    */
    public void draw() {
-      System.out.println("Drawing a rectangle at x: " + getX() + ", y: " + getY() 
-              + "width: " + getWidth() + ", height: " + getHeight());
+       System.out.println("Drawing a rectangle at x: " + getX() + ", y: "
+               + getY() + "width: " + getWidth() + ", height: " + getHeight());
    }
 }
