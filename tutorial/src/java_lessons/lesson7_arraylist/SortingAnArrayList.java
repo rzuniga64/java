7babf283b5d07a12df089b103f8fb39bcde515f1
Raul Zuniga <rzuniga64@gmail.com>
2017-01-20 21:35:12 -0600

    Update.

--- tutorial/src/java_lessons/lesson7_arraylist/SortingAnArrayList.java
+++ tutorial/src/java_lessons/lesson7_arraylist/SortingAnArrayList.java
@@ -4,22 +4,44 @@ import java.util.Random;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class SortingAnArrayList
-{
-   public static void main(String[] args) 
-   {
-      Random rand = new Random(System.currentTimeMillis());
-      ArrayList<Integer> numbers = new ArrayList<Integer>();
-      for(int i = 0; i < 10; ++i)
-         numbers.add(rand.nextInt(101));
-      display(numbers);
-      Collections.sort(numbers);
-      display(numbers);
-   }
-   
-   static void display(ArrayList arr) {
-      for(int i = 0; i < arr.size(); ++i)
-         System.out.print(arr.get(i) + " ");
-      System.out.println();
-   }
+/**
+ * SortingAnArrayList class.
+ */
+public final class SortingAnArrayList {
+
+    /**
+     * Constructor.
+     */
+    private SortingAnArrayList() {
+    }
+
+    /**
+     * Display the array.
+     * @param arr the array
+     */
+    static void display(final ArrayList arr) {
+        for (Object item : arr) {
+            System.out.print(item + " ");
+        }
+        System.out.println();
+    }
+
+    /**
+     * Unit tests.
+     *
+     * @param args the arguments.
+     */
+    public static void main(final String[] args) {
+
+        Random rand = new Random(System.currentTimeMillis());
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        for (int i = 0; i < 10; ++i) {
+            numbers.add(rand.nextInt(101));
+        }
+        display(numbers);
+        Collections.sort(numbers);
+        display(numbers);
+    }
 }
+
