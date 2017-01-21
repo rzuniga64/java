ba9abdfdf6d64b19a5f11d2f9c1818ed1b024e31
Raul Zuniga <rzuniga64@gmail.com>
2017-01-20 20:50:08 -0600

    Update.

--- tutorial/src/java_lessons/lesson7_arraylist/ArrayListIntro.java
+++ tutorial/src/java_lessons/lesson7_arraylist/ArrayListIntro.java
@@ -2,19 +2,33 @@ package java_lessons.lesson7_arraylist;
 
 import java.util.ArrayList;
 
-public class ArrayListIntro
-{
-   public static void main(String[] args)
-   {
+/**
+ * ArrayListIntro class.
+ */
+public final class ArrayListIntro {
+
+    /** Constructor. */
+    private ArrayListIntro() { }
+
+    /**
+     * Unit tests.
+     * @param args the arguments.
+     */
+   public static void main(final String[] args) {
+
       double average;
       int total = 0;
-      ArrayList<Integer> grades = new ArrayList<Integer>();
+
+      ArrayList<Integer> grades = new ArrayList<>();
+
       grades.add(78);
       grades.add(84);
       grades.add(90);
-      for(int grade : grades)
-         total += grade;
+
+      for (int grade : grades) {
+          total += grade;
+      }
       average = total / grades.size();
       System.out.println("The average is " + average);
    }
-}      
+}
