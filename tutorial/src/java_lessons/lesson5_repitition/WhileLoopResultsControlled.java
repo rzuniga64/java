70d887ae6ffc0dc530e7e0c4277255dbb0521d37
Raul Zuniga <rzuniga64@gmail.com>
2017-01-20 18:55:44 -0600

    Update.

--- tutorial/src/java_lessons/lesson5_repitition/WhileLoopResultsControlled.java
+++ tutorial/src/java_lessons/lesson5_repitition/WhileLoopResultsControlled.java
@@ -1,16 +1,27 @@
 package java_lessons.lesson5_repitition;
 
-public class WhileLoopResultsControlled
-{
-   public static void main(String[] args)
-   {
-      double balance = 300000;
-      double rate = 1.10;
-      int years = 0;
-      while (balance <= 1000000) {
-         balance *= rate;
-         ++years;
-      }
-      System.out.println("It will take " + years + " years to reach 100000.");
+/**
+ * WhileLoopResultsControlled class.
+ */
+public final class WhileLoopResultsControlled {
+
+    /** Constructor. */
+    private WhileLoopResultsControlled() { }
+
+    /**
+     * Unit tests.
+     * @param args the arguments.
+     */
+    public static void main(final String[] args) {
+
+        double balance = 300000;
+        double rate = 1.10;
+        int years = 0;
+
+        while (balance <= 1000000) {
+            balance *= rate;
+            ++years;
+        }
+        System.out.println("It will take " + years + " years to reach 100000.");
    }
 }
