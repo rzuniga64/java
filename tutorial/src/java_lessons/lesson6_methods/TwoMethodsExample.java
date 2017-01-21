21e208c7c952841baca3433cb19b55451b7892ea
Raul Zuniga <rzuniga64@gmail.com>
2017-01-20 20:35:59 -0600

    Update.

--- tutorial/src/java_lessons/lesson6_methods/TwoMethodsExample.java
+++ tutorial/src/java_lessons/lesson6_methods/TwoMethodsExample.java
@@ -2,18 +2,30 @@ package java_lessons.lesson6_methods;
 
 import java.util.Scanner;
 
-public class TwoMethodsExample
-{
-   public static void main(String[] args) 
-   {
+/**
+ * MultipleParameters class.
+ */
+public final class TwoMethodsExample {
+
+    /** Constructor. */
+    private TwoMethodsExample() { }
+
+    /**
+     * Unit tests.
+     * @param args the arguments.
+     */
+    public static void main(final String[] args) {
+       double bill, tip;
+       double amt, disc;
+
        Scanner input = new Scanner(System.in);
        Scanner input1 = new Scanner(System.in);
-       /*double bill, tip;
-       System.out.print("Enter the amount of your bill: ");
+
+       /* System.out.print("Enter the amount of your bill: ");
        bill = input.nextDouble();
        tip = tipCalc(bill);
        System.out.println("Your tip should be " + tip + ".");*/
-       double amt, disc;
+
        System.out.print("Enter the original cost: ");
        amt = input.nextDouble();
        System.out.print("Enter the discount in whole number form: ");
@@ -21,11 +33,24 @@ public class TwoMethodsExample
        System.out.println("Your discount is " + discount(amt, disc));
    }
 
-   static double discount(double amount, double percent) {
-      return (amount - (amount * (percent/100)));
-   }   
+    /**
+     * Calculate a discount for an amount.
+     * @param amount the amount
+     * @param percent the percent
+     * @return discount for an amount
+     */
+   private static double discount(final double amount, final double percent) {
 
-   static double tipCalc(double amount) {
-      return amount * .15;
+       return (amount - (amount * (percent / 100)));
+   }
+
+    /**
+     * Calculate the dip on an amount.
+     * @param amount the amount
+     * @return the tip
+     */
+   private static double tipCalc(final double amount) {
+
+       return amount * .15;
    }
 }
