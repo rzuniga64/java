9e210195793b2fa63dd972408aa3a2f194d756b4
Raul Zuniga <rzuniga64@gmail.com>
2017-01-20 20:56:17 -0600

    Update.

--- tutorial/src/java_lessons/lesson7_arraylist/AddingDataToArrayList.java
+++ tutorial/src/java_lessons/lesson7_arraylist/AddingDataToArrayList.java
@@ -2,16 +2,30 @@ package java_lessons.lesson7_arraylist;
 
 import java.util.ArrayList;
 
-public class AddingDataToArrayList
-{
-   public static void main(String[] args) 
-   {
-      ArrayList<Integer> numbers = new ArrayList<Integer>();
-      System.out.println("The size of numbers: " + numbers.size());
-      numbers.add(0);
-      System.out.println("The size of numbers: " + numbers.size());
-      for(int i = 1; i <= 10; ++i)
-         numbers.add(i);
-      System.out.println("The size of numbers: " + numbers.size());
-   }
+/**
+ * AddingDataToArrayList class.
+ */
+public final class AddingDataToArrayList {
+    /**
+     * Constructor.
+     */
+    private AddingDataToArrayList() {
+    }
+
+    /**
+     * Unit tests.
+     *
+     * @param args the arguments.
+     */
+    public static void main(final String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        System.out.println("The size of numbers: " + numbers.size());
+        numbers.add(0);
+        System.out.println("The size of numbers: " + numbers.size());
+        for (int i = 1; i <= 10; ++i) {
+            numbers.add(i);
+        }
+        System.out.println("The size of numbers: " + numbers.size());
+    }
 }
