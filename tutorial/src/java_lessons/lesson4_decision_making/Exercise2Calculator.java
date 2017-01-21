7b24712279f2392e1fa1f42aca112561185bcb88
Raul Zuniga <rzuniga64@gmail.com>
2017-01-20 18:20:48 -0600

    Update.

--- tutorial/src/java_lessons/lesson4_decision_making/Exercise2Calculator.java
+++ tutorial/src/java_lessons/lesson4_decision_making/Exercise2Calculator.java
@@ -2,29 +2,43 @@ package java_lessons.lesson4_decision_making;
 
 import java.util.Scanner;
 
-public class Exercise2Calculator 
-{
-   public static void main(String[] args)
-   {
-      String op;
-      double number1, number2;
-      Scanner inputOp = new Scanner(System.in);
-      Scanner inputNumber = new Scanner(System.in);
-      System.out.print("Enter the first number: ");
-      number1 = inputNumber.nextDouble();
-      System.out.print("Enter the second number: ");
-      number2 = inputNumber.nextDouble();
-      System.out.print("Enter the operator: ");
-      op = inputOp.next();
-      if (op.equals("+"))
-         System.out.println(number1 + number2);
-      else if (op.equals("-"))
-         System.out.println(number1 - number2);
-      else if (op.equals("*"))
-         System.out.println(number1 * number2);
-      else if (op.equals("/"))
-         System.out.println(number1 / number2);
-      else
-         System.out.println("Didn't recognize operator.");
+/**
+ * ExerciseCalculator
+ */
+public final class Exercise2Calculator {
+
+    /** Constructor. */
+    private Exercise2Calculator() { }
+
+    /**
+     * Unit tests.
+     * @param args the arguments.
+     */
+    public static void main(final String[] args) {
+
+        String op;
+        double number1, number2;
+
+        Scanner inputOp = new Scanner(System.in);
+        Scanner inputNumber = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        number1 = inputNumber.nextDouble();
+        System.out.print("Enter the second number: ");
+        number2 = inputNumber.nextDouble();
+        System.out.print("Enter the operator: ");
+        op = inputOp.next();
+
+        if (op.equals("+")) {
+            System.out.println(number1 + number2);
+        } else if (op.equals("-")) {
+            System.out.println(number1 - number2);
+        } else if (op.equals("*")) {
+            System.out.println(number1 * number2);
+        } else if (op.equals("/")) {
+            System.out.println(number1 / number2);
+        } else {
+            System.out.println("Didn't recognize operator.");
+        }
    }
 }
