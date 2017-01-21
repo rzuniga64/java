5e881cc51086a2c643cf1f21a44e154cacc3f33d
Raul Zuniga <rzuniga64@gmail.com>
2017-01-21 04:05:16 -0600

    Update.

--- tutorial/src/java_lessons/lesson10_inheritance/ObjectClass.java
+++ tutorial/src/java_lessons/lesson10_inheritance/ObjectClass.java
@@ -1,12 +1,26 @@
 package java_lessons.lesson10_inheritance;
 
-public class ObjectClass
-{
-   public static void main(String[] args)
-   {
+/**
+ * ObjectClass class.
+ * Every feature in Java is an object and those objects are placed in a chain
+ * of inheritance.
+ * An instance of the Object class can take on any other object.
+ */
+public final class ObjectClass {
+
+   /** Constructor. */
+   private ObjectClass() { }
+
+   /**
+    * Unit tests.
+    * @param args the arguments
+    */
+   public static void main(final String[] args) {
+
       Object anObject = new Employee("Smith", 30000);
-      System.out.println("FileNotFound of anObject: " + anObject.getClass().getName());
-      Employee e1 = (Employee)anObject; // casting
-      System.out.println(e1.displayEmpInfo());
+      System.out.println("Type of Object: "
+              + anObject.getClass().getName());
+      Employee e1 = (Employee) anObject; // casting
+      System.out.println(e1.display());
    }
 }
