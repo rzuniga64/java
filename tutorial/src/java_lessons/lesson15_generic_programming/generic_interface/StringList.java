ab716b12bfe92a4c73ee7594e5da4d100cfac1c0
Raul Zuniga <rzuniga64@gmail.com>
2017-01-22 04:41:22 -0600

    Update.

--- tutorial/src/java_lessons/lesson15_generic_programming/generic_interface/StringList.java
+++ tutorial/src/java_lessons/lesson15_generic_programming/generic_interface/StringList.java
@@ -1,25 +1,50 @@
 package java_lessons.lesson15_generic_programming.generic_interface;
 
+/**
+ *  StringList class.
+ *  @param <T>
+ */
 class StringList<T> implements IList<T> {
+
+    /** The data array. */
     private T[] data;
+    /** The size of the array. */
     private int size;
+    /** The capacity of the array. */
     private int capacity;
 
-    StringList(int numElements) {
-        size = numElements;
+    /** Constructor.
+     * @param newSize the size of the array.
+     */
+    StringList(final int newSize) {
+        size = newSize;
         capacity = 0;
         data = (T[]) new Object[size];
     }
 
-    public void add(T element) {
-        data[capacity] = element;
+    /**
+     * Add an item to the array.
+     * @param item the item to add
+     */
+    public void add(final T item) {
+        data[capacity] = item;
         ++capacity;
     }
 
-    public T get(int n) {
+    /**
+     * Get the nth item of the array.
+     * @param n the index of the item.
+     * @return the nth item of the array
+     */
+    public T get(final int n) {
+
         return data[n];
     }
 
+    /**
+     * String representation of the data.
+     * @return the data
+     */
     public String toString() {
         String elements = "";
         for (int i = 0; i < capacity; ++i) {
