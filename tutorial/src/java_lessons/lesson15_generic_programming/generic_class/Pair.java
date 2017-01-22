4707b8bb273984c4d3b3e2e5832c209829b4f32e
Raul Zuniga <rzuniga64@gmail.com>
2017-01-22 04:23:58 -0600

    Update.

--- tutorial/src/java_lessons/lesson15_generic_programming/generic_class/Pair.java
+++ tutorial/src/java_lessons/lesson15_generic_programming/generic_class/Pair.java
@@ -1,41 +1,57 @@
 package java_lessons.lesson15_generic_programming.generic_class;
 
 /**
- * The type Pair.
- *
- * @param <T1> the type parameter
- * @param <T2> the type parameter
+ *  The Pair class.
+ *  @param <T1> the type parameter
+ *  @param <T2> the type parameter
  */
-class Pair<T1, T2> {
+public final class Pair<T1, T2> {
+
+    /** First gneric item. */
     private T1 firstItem;
+    /** Second generic item. */
     private T2 secondItem;
 
     /**
-     * Instantiates a new Pair.
-     *
-     * @param first  the first data type
-     * @param second the second data type
+     *  Instantiate a new Pair.
+     *  @param first  the first data type
+     *  @param second the second data type
      */
-    Pair(T1 first, T2 second) {
+    private Pair(final T1 first, final T2 second) {
         firstItem = first;
         secondItem = second;
     }
 
     /**
-     * Method to return the first data type
-     *
+     * Method to return the first data type.
      * @return the first data type
      */
     public T1 first() {
+
         return firstItem;
     }
 
     /**
-     * Method to return the second data type
-     *
+     * Method to return the second data type.
      * @return the second data type
      */
     public T2 second() {
+
         return secondItem;
+    }
+
+    /**
+     * Unit tests for Pair class.
+     * @param args the input arguments
+     */
+    public static void main(final String[] args) {
+
+        Pair<String, Integer> grade1 = new Pair<>("Williams", 90);
+        Pair<String, Integer> grade2 = new Pair<>("Brown", 44);
+
+        System.out.println(grade1.first());
+        System.out.println(grade1.second());
+        System.out.println(grade2.first());
+        System.out.println(grade2.second());
     }
 }
