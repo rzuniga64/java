93cf6c7763c6d0b4bcd40bbd7856ee41110ef108
Raul Zuniga <rzuniga64@gmail.com>
2017-01-21 23:36:25 -0600

    Update.

--- tutorial/src/java_lessons/lesson12_arrays/PassArrayAsFuncArgs.java
+++ tutorial/src/java_lessons/lesson12_arrays/PassArrayAsFuncArgs.java
@@ -2,36 +2,70 @@ package java_lessons.lesson12_arrays;
 
 import java.util.Random;
 
-public class PassArrayAsFuncArgs
-{
-   public static void main(String[] args)
-   {
-      final int size = 100;
-      int[] numbers = new int[size];
-      buildArray(numbers, size);
-      displayArr(numbers, size);  
-      System.out.println("The sum of the elements is: " + sumArray(numbers, size));
-   }
+/**
+ *  PassArrayAsFuncArgs class.
+ *  When passing an array as a parameter to a function you must pass the
+ *  array and its size.
+ */
+public final class PassArrayAsFuncArgs {
 
-   private static void buildArray(int[] arr, int size) {
-      Random rand = new Random(System.currentTimeMillis());
-      for(int i = 0; i < size; ++i)
-         arr[i] = rand.nextInt(101);
-   }
+    /** Constructor. */
+    private PassArrayAsFuncArgs() { }
+    /**
+     *  Create an array of random numbers from 1 - 100.
+     *  @param arr the array
+     *  @param size the size of the array
+     */
+    private static void buildArray(final int[] arr, final int size) {
 
-   private static void displayArr(int[] arr, int size) {
-      for(int i = 0; i < size; ++i) {
-         System.out.print(arr[i] + " ");
-         if ((i+1) % 10 == 0)
-            System.out.println();
-      }
-      System.out.println();
-   }
+        Random rand = new Random(System.currentTimeMillis());
+        for (int i = 0; i < size; ++i) {
+            arr[i] = rand.nextInt(101);
+        }
+    }
 
-   private static int sumArray(int[] arr, int size) {
-      int total = 0;
-      for(int i = 0; i < size; ++i)
-         total += arr[i];
-      return total;
+    /**
+     *  Display an array.
+     *  @param arr the array
+     *  @param size the size of the arraay
+     */
+    private static void displayArr(final int[] arr, final int size) {
+
+        for (int i = 0; i < size; ++i) {
+            System.out.print(arr[i] + " ");
+            if ((i + 1) % 10 == 0) {
+                System.out.println();
+            }
+        }
+        System.out.println();
+    }
+
+    /**
+     *  Sum the items in an array.
+     *  @param arr the array
+     *  @param size the size
+     *  @return the sum
+     */
+    private static int sumArray(final int[] arr, final int size) {
+
+        int total = 0;
+        for (int item : arr) {
+            total += item;
+        }
+        return total;
+    }
+
+    /**
+     * Unit tests.
+     * @param args the arguments.
+     */
+    public static void main(final String[] args) {
+
+        final int size = 100;
+        int[] numbers = new int[size];
+        buildArray(numbers, size);
+        displayArr(numbers, size);
+        System.out.println("The sum of the elements is: "
+                + sumArray(numbers, size));
    }
 }
