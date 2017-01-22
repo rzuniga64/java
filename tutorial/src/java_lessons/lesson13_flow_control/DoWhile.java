141acbcca54149783e1e15af70e27a9f00c5a82b
Raul Zuniga <rzuniga64@gmail.com>
2017-01-22 01:27:24 -0600

    Update.

--- tutorial/src/java_lessons/lesson13_flow_control/DoWhile.java
+++ tutorial/src/java_lessons/lesson13_flow_control/DoWhile.java
@@ -1,19 +1,37 @@
 package java_lessons.lesson13_flow_control;
 
-public class DoWhile
-{
-   /*
-   do {
-      set of statements;
-   } while(relational expression);
- 
-   */
-   public static void main(String[] args)
-   {
+/**
+ * DoWhile class.
+ *  do {
+ *      set of statements;
+ *      } while(relational expression);
+*/
+public final class DoWhile {
+
+    /** Constructor. */
+    private DoWhile() { }
+
+    /**
+    * Unit tests.
+    * @param args the arguments
+    */
+   public static void main(final String[] args) {
+
       int number = 1;
       do {
          System.out.println(number);
          ++number;
-      } while(number < 11); // (number <= 10)
+      } while (number < 11); // (number <= 10)
+
+       double balance, rate;
+       int years = 0;
+       balance = 400000;
+       rate = 1.10;
+       do {
+           balance *= rate;
+           ++years;
+       } while (balance < 1000000);
+       System.out.println("It will take " + years
+               + " years to reach $1,000,000.");
    }
 }
