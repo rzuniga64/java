58f40c73f0c692dbbcba7273041b3a835319578b
Raul Zuniga <rzuniga64@gmail.com>
2017-01-22 00:18:17 -0600

    Update.

--- tutorial/src/java_lessons/lesson12_arrays/ArrayElementsAsCounters.java
+++ tutorial/src/java_lessons/lesson12_arrays/ArrayElementsAsCounters.java
@@ -18,26 +18,23 @@ public final class ArrayElementsAsCounters {
      */
    public static void main(final String[] args) {
 
+       Random rand = new Random(System.currentTimeMillis());
        final int size = 101;
        int[] numbers = new int[size];
+       // Store the number of occurrences in the first array.
        int[] distribution = new int[size];
-       Random rand = new Random(System.currentTimeMillis());
 
+       // Create array of random numbers from 1 - 100.
        for (int i = 0; i < size; ++i) {
            numbers[i] = rand.nextInt(100 - 1 + 1) + 1;
        }
 
+       // Count the distribution of numbers in the numbers array.
        for (int i = 0; i < size; ++i) {
            ++distribution[numbers[i]];
        }
 
-       for (int i = 0; i < size; ++i) {
-            System.out.print(numbers[i] + " ");
-            if ((i + 1) % 10 == 0) {
-                System.out.println();
-            }
-       }
-
+       // Print out the list of distribution.
        for (int i = 1; i < size; ++i) {
            if (distribution[i] != 0) {
                System.out.println("Number " + i + " occurs " + distribution[i]
