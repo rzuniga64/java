aac87b9694efbbfc502fdb11de27c52814292aae
Raul Zuniga <rzuniga64@gmail.com>
2017-01-22 03:02:21 -0600

    Update.

--- tutorial/src/java_lessons/lesson14_file_processing/WritingCharsToFile.java
+++ tutorial/src/java_lessons/lesson14_file_processing/WritingCharsToFile.java
@@ -19,9 +19,12 @@ public final class WritingCharsToFile {
      */
    public static void main(final String[] args) throws IOException {
 
-      String fileName = "chars.txt";
+       String fileName = "tutorial\\src\\java_lessons\\lesson14_file_processing"
+               + "\\chars.txt";
+
       PrintWriter outFile = new PrintWriter(new FileWriter(fileName));
       String sentence = "now is the time for all good people";
+
       for (int i = 0; i < sentence.length(); ++i) {
          outFile.write(sentence.charAt(i));
          outFile.write('\n');
