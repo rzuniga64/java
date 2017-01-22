04cafea82d8cadc99a79bdc650eb2dbe9980ec5a
Raul Zuniga <rzuniga64@gmail.com>
2017-01-22 01:58:20 -0600

    Update.

--- tutorial/src/java_lessons/lesson13_flow_control/DoWhileExercise.java
+++ tutorial/src/java_lessons/lesson13_flow_control/DoWhileExercise.java
@@ -3,27 +3,36 @@ package java_lessons.lesson13_flow_control;
 import java.util.Scanner;
 import java.util.Random;
 
-public class DoWhileExercise
-{
-   public static void main(String[] args)
-   {
-      Scanner inputNum = new Scanner(System.in);
-      Scanner input = new Scanner(System.in);
-      Random rand = new Random(System.currentTimeMillis());
-      String contChar;
-      int number, guess;
-      number = rand.nextInt(11);
-      char cont = 'y';
-      do {
-         System.out.print("Guess a number: ");
-         guess = inputNum.nextInt();
-         if (guess == number) {
-            System.out.println("That's right!");
-            break;
-         }
-         System.out.print("Sorry. Do you want to guess again? (y/n) ");
-         contChar = input.nextLine();
-         cont = contChar.charAt(0);
-      } while (cont != 'n');
-   }
+/**
+ * DoWhileExercise.
+ */
+public final class DoWhileExercise {
+
+    /** Constructor. */
+    private DoWhileExercise() { }
+
+    /**
+     * Unit tests.
+     * @param args the arguments.
+     */
+    public static void main(final String[] args) {
+        Scanner inputNum = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+        Random rand = new Random(System.currentTimeMillis());
+        String contChar;
+        int number, guess;
+        number = rand.nextInt(11);
+        char cont;
+        do {
+            System.out.print("Guess a number: ");
+            guess = inputNum.nextInt();
+            if (guess == number) {
+                System.out.println("That's right!");
+                break;
+            }
+            System.out.print("Sorry. Do you want to guess again? (y/n) ");
+            contChar = input.nextLine();
+            cont = contChar.charAt(0);
+        } while (cont != 'n');
+    }
 }
