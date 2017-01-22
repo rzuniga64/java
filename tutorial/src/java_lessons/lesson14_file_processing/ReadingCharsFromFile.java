a242ca071cd6f75fd1786e6b38c9c42f66638d08
Raul Zuniga <rzuniga64@gmail.com>
2017-01-22 02:51:59 -0600

    Update.

--- tutorial/src/java_lessons/lesson14_file_processing/ReadingCharsFromFile.java
+++ tutorial/src/java_lessons/lesson14_file_processing/ReadingCharsFromFile.java
@@ -2,21 +2,37 @@ package java_lessons.lesson14_file_processing;
 
 import java.io.*;
 
-public class ReadingCharsFromFile
-{
-   public static void main(String[] args) throws IOException
-   {
-      String inputFile = "chars.txt";
-      String outputFile = "chars.bak";
-      FileReader inFile = new FileReader(inputFile);
-      FileWriter outFile = new FileWriter(outputFile);
-      int ch = 0;
-      ch = inFile.read();
-      while(ch != -1) {
-          outFile.write(ch);
-          ch = inFile.read();
-      }
-      outFile.close();
-      inFile.close();
-   }
+/**
+ *  ReadingCharsFromFile class.
+ *  FileWriter opens a file for output.
+ *  FileWriter opens a file for reading.
+ *  Must add throws IOException to the main function.
+ */
+public final class ReadingCharsFromFile {
+
+    /** Constructor. */
+    private ReadingCharsFromFile() { }
+
+    /**
+     * Unit tests.
+     * @param args the arguments
+     * @throws IOException IOException
+     */
+    public static void main(final String[] args) throws IOException {
+
+        String inputFile = "chars.txt";
+        String outputFile = "chars.bak";
+        FileReader inFile = new FileReader(inputFile);
+        FileWriter outFile = new FileWriter(outputFile);
+        int ch;
+        ch = inFile.read();
+
+        while (ch != -1) {
+            outFile.write(ch);
+            ch = inFile.read();
+        }
+
+        outFile.close();
+        inFile.close();
+    }
 }
