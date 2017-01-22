07f7b94e6c2b716ee1d88f45fa467fd9add5a30c
Raul Zuniga <rzuniga64@gmail.com>
2017-01-22 00:55:34 -0600

    Update.

--- tutorial/src/java_lessons/lesson13_flow_control/SwitchStatementExercise.java
+++ tutorial/src/java_lessons/lesson13_flow_control/SwitchStatementExercise.java
@@ -1,45 +1,65 @@
 package java_lessons.lesson13_flow_control;
 
-public class SwitchStatementExercise
-{
-   public static void main(String[] args) 
-   {
-      double n1, n2;
-      n1 = 23;
-      n2 = 11;
-      System.out.println(calc(n1, n2, '+'));
-      System.out.println(calc(n1, n2, '-'));
-      System.out.println(calc(n1, n2, '*'));
-      System.out.println(calc(n1, n2, '/'));
-   }
-   static double calc(double num1, double num2, char op) {
-      /*if (op == '+')
-         return num1 + num2;
-      else if (op == '-')
-         return num1 - num2;
-      else if (op == '*')
-         return num1 * num2;
-      else if (op == '/')
-         return num1 / num2;
-      else
-         return 0; */
-      double result = 0.0;
-      switch(op) {
-         case '+':
-            result = num1 + num2;
-            break;
-         case '-':
-            result = num1 - num2;
-            break;
-         case '*':
-            result = num1 * num2;
-            break;
-         case '/':
-            result = num1 / num2;
-            break;
-         default:
-            return 0;
-      }
-      return result;
+/**
+ * SwitchStatementExercise class.
+ */
+public final class SwitchStatementExercise {
+
+    /** Constructor. */
+    private SwitchStatementExercise() { }
+
+    /**
+     * Unit tests.
+     * @param args the arguments.
+     */
+    public static void main(final String[] args) {
+
+        double n1, n2;
+        n1 = 23;
+        n2 = 11;
+        System.out.println(calc(n1, n2, '+'));
+        System.out.println(calc(n1, n2, '-'));
+        System.out.println(calc(n1, n2, '*'));
+        System.out.println(calc(n1, n2, '/'));
+    }
+
+    /**
+     *  Operation function.
+     *  @param num1 number
+     *  @param num2 number
+     *  @param op operation
+     *  @return the result of the calculation
+     */
+    private static double calc(final double num1, final double num2,
+                               final char op) {
+        /*if (op == '+')
+            return num1 + num2;
+        else if (op == '-')
+            return num1 - num2;
+        else if (op == '*')
+            return num1 * num2;
+        else if (op == '/')
+            return num1 / num2;
+        else
+            return 0; */
+
+        double result;
+        switch (op) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                return 0;
+        }
+        return result;
    }
 }
