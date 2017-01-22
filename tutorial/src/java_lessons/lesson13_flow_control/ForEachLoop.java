3979dd9eeecebf66acc7b3efb7581abe68da1657
Raul Zuniga <rzuniga64@gmail.com>
2017-01-22 01:39:17 -0600

    Update.

--- tutorial/src/java_lessons/lesson13_flow_control/ForEachLoop.java
+++ tutorial/src/java_lessons/lesson13_flow_control/ForEachLoop.java
@@ -1,28 +1,47 @@
 package java_lessons.lesson13_flow_control;
 
-//import java.util.ArrayList;
+import java.util.ArrayList;
 
-/*
-for(variable declaration : collection-name) {
-    set of statements;
-}
+/**
+ *  ForEachLoop class.
+ *  for(variable declaration : collection-name) {
+ *      set of statements;
+ *  }
 */
+public final class ForEachLoop {
 
+    /** Constructor. */
+    private ForEachLoop() { }
 
-public class ForEachLoop
-{
-   public static void main(String[] args)
-   {
-      /*ArrayList<Integer> numbers = new ArrayList<Integer>();
-      for(int i = 1; i < 11; ++i)
-         numbers.add(i);
-      for(int num : numbers)
-         System.out.print(num + " ");*/
-      int[] numbers = new int[10];
-      for(int i = 0; i < 10; ++i)
-         numbers[i] = i+1;
-      for(int num : numbers)
-         System.out.print(num + " ");
-   }
+    /**
+     * Unit tests.
+     * @param args the arguments.
+     */
+    public static void main(final String[] args) {
+
+        int[] numbers = new int[10];
+        // initialize the array.
+        for (int i = 0; i < 10; ++i) {
+            numbers[i] = i + 1;
+        }
+        // print the array.
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+
+        ArrayList<Employee> emps = new ArrayList<>();
+
+        Employee emp1 = new Employee("Brown", 20000);
+        Employee emp2 = new Employee("Smith", 21000);
+        Employee emp3 = new Employee("Jones", 22000);
+        Employee emp4 = new Employee("Green", 23000);
+        emps.add(emp1);
+        emps.add(emp2);
+        emps.add(emp3);
+        emps.add(emp4);
+
+        for (Employee emp : emps) {
+            System.out.println(emp.displayEmpInfo());
+        }
+    }
 }
-        
