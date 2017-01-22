7315a80aae0361542382c9e6836382870b718763
Raul Zuniga <rzuniga64@gmail.com>
2017-01-22 03:05:11 -0600

    Update.

--- tutorial/src/java_lessons/lesson14_file_processing/WritingCharsToFile.java
+++ tutorial/src/java_lessons/lesson14_file_processing/WritingCharsToFile.java
@@ -25,13 +25,13 @@ public final class WritingCharsToFile {
        String fileName = "tutorial\\src\\java_lessons\\lesson14_file_processing"
                + "\\chars.txt";
 
-      PrintWriter outFile = new PrintWriter(new FileWriter(fileName));
-      String sentence = "now is the time for all good people";
+       PrintWriter outFile = new PrintWriter(new FileWriter(fileName));
+       String sentence = "now is the time for all good people";
 
-      for (int i = 0; i < sentence.length(); ++i) {
-         outFile.write(sentence.charAt(i));
-         outFile.write('\n');
-      }
-      outFile.close();
+       for (int i = 0; i < sentence.length(); ++i) {
+            outFile.write(sentence.charAt(i));
+            outFile.write('\n');
+        }
+        outFile.close();
    }
 }
