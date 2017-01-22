48274120281beb2ab68eb683d0e4281745d7f57d
Raul Zuniga <rzuniga64@gmail.com>
2017-01-21 23:17:23 -0600

    Update.

--- tutorial/src/java_lessons/lesson12_arrays/AccessingArrayElements.java
+++ tutorial/src/java_lessons/lesson12_arrays/AccessingArrayElements.java
@@ -1,26 +1,32 @@
 package java_lessons.lesson12_arrays;
 
-// We will access array elements and initialize
-// an array that has been declared but not initialized.
-public class AccessingArrayElements
-{
-   public static void main(String[] args)
-   {
+/**
+ * The AccessingArrayElementsClass.
+  */
+public final class AccessingArrayElements {
+
+    /** Constructor. */
+    private AccessingArrayElements() { }
+
+    /**
+     * Unit tests.
+     * @param args the arguments
+     */
+    public static void main(final String[] args) {
+
       final int size = 10;
-      // declare an array 
-      int[] numbers = new int[size];  
-      // initialize the array 
-      for(int i = 0; i < size; ++i)
-         numbers[i] = i+1;
-      // access the array
-      for(int number: numbers)
-         System.out.print(number + " ");
-      // let's total the array
+      int[] numbers = new int[size];
       int total = 0;
-      for(int i = 0; i < size; ++i)
-         total += numbers[i];
-      System.out.println("Total: " + total);
-      // out of bounds exception (run-time error)
-      // System.out.print("Out of bounds: " + numbers[10]);
+
+      for (int i = 0; i < size; ++i) {
+          numbers[i] = i + 1;
+      }
+      // access the array
+      for (int number: numbers) {
+          System.out.print(number + " ");
+          total += number;
+      }
+
+      System.out.println("The total is " + total);
    }
 }
