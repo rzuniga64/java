0bd26895e8c6e387c45d7f2645d0fd11908c1951
Raul Zuniga <rzuniga64@gmail.com>
2017-01-22 17:34:17 -0600

    Update.

--- tutorial/src/java_lessons/lesson18_data_structures/BinarySearchTree.java
+++ tutorial/src/java_lessons/lesson18_data_structures/BinarySearchTree.java
@@ -4,6 +4,13 @@ package java_lessons.lesson18_data_structures;
  * The type Binary search tree class. One of the advantages is that it makes it
  * easy to find data that is unsorted.
  *
+ *  A binary tree is a tree data structure in which each node has at most two
+ *  children, which are referred to as the left child and the right child.
+ *  A recursive definition using just set theory notions is that a (non-empty)
+ *  binary tree is a triple (L, S, R), where L and R are binary trees or the
+ *  empty set and S is a singleton set.[1] Some authors allow the binary tree
+ *  to be the empty set as well. - Wikipedia
+ *
  * Methods:
  * - insert(e)
  * - inorder()
