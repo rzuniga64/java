4e6e39fb2980282da5ac8670e028552c74edebe5
Raul Zuniga <rzuniga64@gmail.com>
2017-01-22 14:27:00 -0600

    Update.

--- tutorial/src/java_lessons/lesson16_sequential_collections/hashSet.java
+++ tutorial/src/java_lessons/lesson16_sequential_collections/hashSet.java
@@ -4,34 +4,38 @@ import java.util.HashSet;
 
 /**
  * The type Hash set. A set is an unordered collection of unique objects.
- * The HashSet will NOT store data in a natural order.
- * Example, alphabetic order if data are strings or ascending order if data are numbers.
+ * The HashSet will NOT store data in a natural order.For example, alphabetic
+ * order if data are strings or ascending order if data are numbers.
  */
-public class hashSet {
+public final class hashSet {
+
+    /** Constructor. */
+    private hashSet() { }
+
     /**
-     * The entry point of application.
-     *
+     * Unit Tests for hashSet class.
      * @param args the input arguments
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
 
         HashSet<String> names = new HashSet<>();
         names.add("David");
         names.add("Bryan");
         names.add("Cynthia");
+        names.add("David");
         System.out.println("The number of names is " + names.size());
 
-        for (String name : names)
+        for (String name : names) {
             System.out.println(name);
-
+        }
         names.remove("David");
         names.add("Raymond");
         String name = "David";
-        if (names.contains("name"))
+        if (names.contains("name")) {
             System.out.println(name + " is in the collection.");
-        else
+        } else {
             System.out.println(name + " is not in the collection.");
-
+        }
         names.clear();
         System.out.println("The number of names is " + names.size());
     }
