aa9ec54e92141d88626774ab586a1be145f1a3bf
Raul Zuniga <rzuniga64@gmail.com>
2017-01-22 02:10:31 -0600

    Update.

--- tutorial/src/java_lessons/lesson13_flow_control/ForEachLoopExercise.java
+++ tutorial/src/java_lessons/lesson13_flow_control/ForEachLoopExercise.java
@@ -3,18 +3,32 @@ package java_lessons.lesson13_flow_control;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class ForEachLoopExercise
-{
-   public static void main(String[] args)
-   {
-      ArrayList<Integer> numbers = new ArrayList<Integer>();
-      for(int i = 1; i < 11; ++i)
-         numbers.add(i);
-      for(Iterator<Integer> n = numbers.iterator(); n.hasNext();) {
-         int num = n.next();
-         System.out.println(num);
-      }
-      for(int n : numbers)
-         System.out.println(n);
-   }
+/**
+ * ForEachLoopExercise class.
+ */
+public final class ForEachLoopExercise {
+
+    /** Constructor. */
+    private ForEachLoopExercise() { }
+
+    /**
+     * Unit tests.
+     * @param args the arguments.
+     */
+    public static void main(final String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i < 11; ++i) {
+            numbers.add(i);
+        }
+
+        for (Iterator<Integer> n = numbers.iterator(); n.hasNext();) {
+            int num = n.next();
+            System.out.println(num);
+        }
+
+        for (int n : numbers) {
+            System.out.println(n);
+        }
+    }
 }
