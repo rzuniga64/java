bc9db372f633f0749374a99a2e2348794015aaec
Raul Zuniga <rzuniga64@gmail.com>
2017-01-22 17:18:19 -0600

    Update.

--- tutorial/src/java_lessons/lesson18_data_structures/StackEx.java
+++ tutorial/src/java_lessons/lesson18_data_structures/StackEx.java
@@ -5,10 +5,21 @@ import java.util.Stack;
 /**
  *  The StackEx class. Items stored in a stack is stored as an Object data type
  *  so the data type does not need to be specified.
+ *
+ *  The Stack class represents a last-in-first-out (LIFO) stack of objects.
+ *  It extends class Vector with five operations that allow a vector to be
+ *  treated as a stack. The usual push and pop operations are provided, as well
+ *  as a method to peek at the top item on the stack, a method to test for
+ *  whether the stack is empty, and a method to search the stack for an item
+ *  and discover how far it is from the top. When a stack is first created,
+ *  it contains no items.
+ *
  *  Methods used:
  *  - push(e)
  *  - pop()
+ *  - peek()
  *  - empty()
+ *  - search()
  */
 public final class StackEx {
 
