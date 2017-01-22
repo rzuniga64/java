e978d01ffc8f82ca1df690bb5e45512bff55bbca
Raul Zuniga <rzuniga64@gmail.com>
2017-01-22 02:26:48 -0600

    Update.

--- tutorial/src/java_lessons/lesson14_file_processing/WritingDataToFile.java
+++ tutorial/src/java_lessons/lesson14_file_processing/WritingDataToFile.java
@@ -1,15 +1,28 @@
+package java_lessons.lesson14_file_processing;
+
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.io.IOException;
 
-public class WritingDataToFile
-{
-   public static void main(String[] args) throws IOException
-   {
-      String fileName = "grades.txt";
-      PrintWriter outFile = new PrintWriter(new FileWriter(fileName));
-      outFile.println(85);
-      outFile.println(77);
-      outFile.close();
-   }
+/**
+ * WritingDataToFile class.
+ */
+public final class WritingDataToFile {
+
+    /** Constructor. */
+    private WritingDataToFile() { }
+
+    /**
+     * Unit tests.
+     * @param args the arguments
+     * @throws IOException
+     */
+    public static void main(final String[] args) throws IOException {
+
+        String fileName = "grades.txt";
+        PrintWriter outFile = new PrintWriter(new FileWriter(fileName));
+        outFile.println(85);
+        outFile.println(77);
+        outFile.close();
+    }
 }
