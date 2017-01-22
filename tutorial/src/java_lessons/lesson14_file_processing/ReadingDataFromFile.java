71af1e8f919700d9608a791a0b22fae1e80a3bc4
Raul Zuniga <rzuniga64@gmail.com>
2017-01-22 02:37:10 -0600

    Update.

--- tutorial/src/java_lessons/lesson14_file_processing/ReadingDataFromFile.java
+++ tutorial/src/java_lessons/lesson14_file_processing/ReadingDataFromFile.java
@@ -4,13 +4,25 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class ReadingDataFromFile
-{
-   public static void main(String[] args) throws IOException
-   {
+/**
+ *  ReadingDataFromFile class.
+ */
+public final class ReadingDataFromFile {
+
+    /** Constructor. */
+    private ReadingDataFromFile() { }
+
+    /**
+     * Unit tests.
+     * @param args the arguments.
+     * @throws IOException IOException
+     */
+   public static void main(final String[] args) throws IOException {
+
       String fileName = "grades.txt";
       Scanner inFile = new Scanner(new FileReader(fileName));
       int grade;
+
       while (inFile.hasNextLine()) {
          grade = Integer.parseInt(inFile.nextLine());
          System.out.println(grade);
