bf6135c5a337d93b9acd819015e20d9a37b34fc1
Raul Zuniga <rzuniga64@gmail.com>
2017-01-22 02:55:31 -0600

    Update.

--- tutorial/src/java_lessons/lesson14_file_processing/AppendingDataToFile.java
+++ tutorial/src/java_lessons/lesson14_file_processing/AppendingDataToFile.java
@@ -5,21 +5,34 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class AppendingDataToFile
-{
-   public static void main(String[] args) throws IOException
-   {
-      String fileName = "grades.txt";
-      PrintWriter outFile = new PrintWriter(new FileWriter(fileName, true));
-      Scanner input = new Scanner(System.in);
-      int grade = 0;
-      System.out.print("Enter a grade (-1 to quit): ");
-      grade = input.nextInt();
-      while (grade != -1) {
-         outFile.println(grade);
-          System.out.print("Enter a grade (-1 to quit): ");
-          grade = input.nextInt();
-      }
-      outFile.close();
-   }
+/**
+ * AppendingDataToFile class.
+ */
+public final class AppendingDataToFile {
+
+   /** Constructor. */
+   private AppendingDataToFile() { }
+
+   /**
+    * Unit tests.
+    * @param args the arguments
+    * @throws IOException IOException
+    */
+   public static void main(final String[] args) throws IOException {
+
+       String fileName = "tutorial\\src\\java_lessons\\lesson14_file_processing"
+              + "\\grades.txt";
+        PrintWriter outFile = new PrintWriter(new FileWriter(fileName, true));
+        Scanner input = new Scanner(System.in);
+        int grade;
+        System.out.print("Enter a grade (-1 to quit): ");
+        grade = input.nextInt();
+
+        while (grade != -1) {
+            outFile.println(grade);
+            System.out.print("Enter a grade (-1 to quit): ");
+            grade = input.nextInt();
+        }
+        outFile.close();
+    }
 }
