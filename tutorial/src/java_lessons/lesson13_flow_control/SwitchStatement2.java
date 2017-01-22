427502ebd69fd0811e853187452d38ba41620cff
Raul Zuniga <rzuniga64@gmail.com>
2017-01-22 00:49:57 -0600

    Update.

--- tutorial/src/java_lessons/lesson13_flow_control/SwitchStatement2.java
+++ tutorial/src/java_lessons/lesson13_flow_control/SwitchStatement2.java
@@ -2,25 +2,35 @@ package java_lessons.lesson13_flow_control;
 
 import java.util.Scanner;
 
-public class SwitchStatement2
-{
-   public static void main(String[] args) 
-   {
-      char letter;
-      String letterGrade;
-      Scanner input = new Scanner(System.in);
-      System.out.print("Enter a letter grade: ");
-      letterGrade = input.nextLine();
-      letter = letterGrade.charAt(0);
-      switch(letter) {
-         case 'A':
-         case 'B':
-         case 'C':
-            System.out.println("Pass");
-            break;
-         default:
-            System.out.println("Fail");
-      }
-   }
+/**
+ * SwitchStatement2 class.
+ */
+public final class SwitchStatement2 {
+
+    /** Constructor. */
+    private SwitchStatement2() { }
+
+    /**
+     * Unit tests.
+     * @param args the arguments
+     */
+    public static void main(final String[] args) {
+
+        char letter;
+        String letterGrade;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a letter grade: ");
+        letterGrade = input.nextLine();
+        letter = letterGrade.charAt(0);
+
+        switch (letter) {
+            case 'A':
+            case 'B':
+            case 'C':
+                System.out.println("Pass");
+                break;
+            default:
+                System.out.println("Fail");
+        }
+    }
 }
-       
