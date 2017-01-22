9010f1ee6485ea61f0f66515f0760d2080590c7d
Raul Zuniga <rzuniga64@gmail.com>
2017-01-22 02:19:37 -0600

    Update.

--- tutorial/src/java_lessons/lesson14_file_processing/TryCatchStatement.java
+++ tutorial/src/java_lessons/lesson14_file_processing/TryCatchStatement.java
@@ -2,40 +2,46 @@ package java_lessons.lesson14_file_processing;
 
 import java.util.Scanner;
 
-/* 
-
-try {
-   set of statements;
-}
-catch (Exception object) {
-   set of statements;
-}
-catch (Exception object) {
-   set of statements;
-}
+/**
+ * TryCatchStatement class.
+ * try {
+ *  set of statements;
+ *  }
+ *  catch (Exception object) {
+ *  set of statements;
+ *  }
+ *  catch (Exception object) {
+ *  set of statements;
+ *  }
 */
+public final class TryCatchStatement {
 
-public class TryCatchStatement
-{
-   static int numerator, denominator, result;
-   static Scanner input = new Scanner(System.in);
+    /** Numerator, denomator, result. */
+    private static int numerator, denominator, result;
+    /** Input from the keyboard. */
+    private static Scanner input = new Scanner(System.in);
 
-   public static void main(String[] args)
-   {
-      try {
-         System.out.print("Enter the numerator: ");
-         numerator = input.nextInt();
-         System.out.print("Enter the denominator: ");
-         denominator = input.nextInt();
-         result = numerator / denominator;
-         System.out.println("The result is " + result);
-      }
-      catch (ArithmeticException ae) {
-         System.out.println("You tried to divide by zero. Try again.");
-         System.out.print("Enter a new denominator: ");
-         denominator = input.nextInt();
-         result = numerator / denominator;
-         System.out.println("The result is " + result);
-      }
-   }
+    /** Constructor. */
+    private TryCatchStatement() { }
+    /**
+     * Unit tests.
+     * @param args the arguments.
+     */
+    public static void main(final String[] args) {
+
+        try {
+            System.out.print("Enter the numerator: ");
+            numerator = input.nextInt();
+            System.out.print("Enter the denominator: ");
+            denominator = input.nextInt();
+            result = numerator / denominator;
+            System.out.println("The result is " + result);
+        } catch (ArithmeticException ae) {
+            System.out.println("You tried to divide by zero. Try again.");
+            System.out.print("Enter a new denominator: ");
+            denominator = input.nextInt();
+            result = numerator / denominator;
+            System.out.println("The result is " + result);
+        }
+    }
 }
