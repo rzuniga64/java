5b2030b39b4d7afbdfe5f555700218341aad524b
Raul Zuniga <rzuniga64@gmail.com>
2017-01-22 17:45:45 -0600

    Update.

--- tutorial/src/java_lessons/lesson18_data_structures/StackConvert.java
+++ tutorial/src/java_lessons/lesson18_data_structures/StackConvert.java
@@ -3,38 +3,43 @@ package java_lessons.lesson18_data_structures;
 import java.util.Stack;
 
 /**
- * This class uses a Stack to convert decimal numbers to binary.
+ *  StackConvert class.
+ *  This class uses a Stack to convert decimal numbers to binary.
  */
-public class StackConvert {
+public final class StackConvert {
+
+    /** Constructor. */
+    private StackConvert() { }
+
     /**
-     * this method converts a decimal number to binary format.
+     * Convert a decimal number to binary format.
      *
      * @param number an integer number
      * @return the binary number as a String
      */
-    static String binary(int number) {
+    private static String binary(int number) {
 
         final int base = 2;
         Stack digits = new Stack();
 
         do {
             digits.push(number % base);  // this will be a bit or a binary digit
-            number /= base; // divide number by base to continue on to next place holder
+            // divide number by base to continue on to next place holder
+            number /= base;
         } while (number != 0);
 
         String bits = "";
-        while(!digits.empty())
+        while (!digits.empty()) {
             bits += digits.pop();
-
+        }
         return bits;
     }
 
     /**
-     * The entry point of application.
-     *
+     * Unit tests for StackConvert class.
      * @param args the input arguments
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
 
         int num = 5; //101
         System.out.println(num + " in binary is " + binary(num));
