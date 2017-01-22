e257e082779906852f8841bba47c0ee97820469d
Raul Zuniga <rzuniga64@gmail.com>
2017-01-22 04:37:02 -0600

    Update.

--- tutorial/src/java_lessons/lesson15_generic_programming/generic_interface/NumberList.java
+++ tutorial/src/java_lessons/lesson15_generic_programming/generic_interface/NumberList.java
@@ -1,25 +1,51 @@
 package java_lessons.lesson15_generic_programming.generic_interface;
 
-class NumberList<T> implements IList<T> {
+/**
+ *  NumberList class.
+ *  @param <T>
+ */
+public final class NumberList<T> implements IList<T> {
+
+    /** The data array. */
     private T[] data;
+    /** The size of the array. */
     private int size;
+    /** The capacity of the array. */
     private int capacity;
 
-    NumberList(int numElements) {
-        size = numElements;
+    /**
+     * Constructor.
+     * @param numItems the number of items
+     */
+    NumberList(final int numItems) {
+        size = numItems;
         capacity = 0;
         data = (T[]) new Object[size];
     }
 
-    public void add(T element) {
-        data[capacity] = element;
+    /**
+     * Add an item to the array.
+     * @param item the item to add
+     */
+    public void add(final T item) {
+        data[capacity] = item;
         ++capacity;
     }
 
-    public T get(int n) {
+    /**
+     * Get the nth item of the array.
+     * @param n the index of the item.
+     * @return the nth item of the array
+     */
+    public T get(final int n) {
+
         return data[n];
     }
 
+    /**
+     * String representation of the data.
+     * @return the data
+     */
     public String toString() {
         String elements = "";
         for (int i = 0; i < capacity; ++i) {
