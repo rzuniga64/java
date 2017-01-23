617871c5ed21f73906abeac08dc0a9bfce6f42d2
Raul Zuniga <rzuniga64@gmail.com>
2017-01-22 21:20:20 -0600

    Update.

--- tutorial/src/java_lessons/lesson19_sort_and_search/search/LinearSearch.java
+++ tutorial/src/java_lessons/lesson19_sort_and_search/search/LinearSearch.java
@@ -1,53 +1,61 @@
 package java_lessons.lesson19_sort_and_search.search;
 
 /**
- * The type Linear search.
+ * The LineaSearch class.
+ *
+ *  A linear search starts at the first element of a data collection and
+ *  searches element by element for a particular item.
+ *
+ *  Runtime analysis
+ *  Best    Average     Worst
+ *  O(1)    O(n)      O(n)
  */
-public class LinearSearch {
+public final class LinearSearch {
+
+    /** Constructor. */
+    private LinearSearch() { }
 
     /**
-     *  This method performs a Linear search. A Linear search can work on unsorted data.
+     *  This method performs a Linear search. A Linear search can work on
+     *  unsorted data.
      *
      *  @param arr the arr
      *  @param key the key
      *  @return the int
-     *
-     *  A  linear search starts at the first element of a data collection and searches element by element for a
-     *  particular item.
-     *
-     *  Runtime analysis
-     *  Best    Average     Worst
-     *  O(1)    O(n)      O(n)
      */
-    static int linearSearch(int[] arr, int key) {
+    static int linearSearch(final int[] arr, final int key) {
+
         for (int i = 1; i < arr.length; i++) {
-            if (arr[i] == key)
+            if (arr[i] == key) {
                 return i;
+            }
         }
         return -1;
     }
 
     /**
-     * This method performs a Linear search to find the minimum value of a data list.
-     *
+     * This method performs a Linear search to find the minimum value of a data
+     * list.
      * @param arr the arr
      * @return the int
      */
-    private static int min(int[] arr) {
+    private static int min(final int[] arr) {
+
         int minimum = arr[0];
         for (int i = 1; i < arr.length; i++) {
-            if (arr[i] < minimum)
+            if (arr[i] < minimum) {
                 minimum = arr[i];
+            }
         }
         return minimum;
     }
 
     /**
-     * The entry point of application.
-     *
+     * Unit tests for LinearSearch class.
      * @param args the input arguments
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
+
         int[] data = new int[]{5, 10, 1, 9, 4, 8, 3, 6, 2, 7};
         int minimum = min(data);
         System.out.println("The minimum value is: " + minimum);
