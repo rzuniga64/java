53308a87452f24d0e26d380573d922414765b798
Raul Zuniga <rzuniga64@gmail.com>
2018-09-14 06:15:19 -0500

    Update interpreter pattern files.

--- design_patterns/src/behavorial/interpreter/Cups.java
+++ design_patterns/src/behavorial/interpreter/Cups.java
@@ -5,26 +5,51 @@ package behavorial.interpreter;
  */
 public class Cups extends Expression{
 
+    /**
+     *  Convert cups to gallons.
+     *  @param quantity
+     *  @return the number of gallons
+     */
     public String gallons(double quantity) {
 
         return Double.toString(quantity/16);
     }
 
+    /**
+     *  Convert cups to quarts
+     *  @param quantity
+     *  @return the number of quarts.
+     */
     public String quarts(double quantity) {
 
         return Double.toString(quantity/4);
     }
 
+    /**
+     *  Convert cups to pints.
+     *  @param quantity
+     *  @return the number of pints.
+     */
     public String pints(double quantity) {
 
         return Double.toString(quantity/2);
     }
 
+    /**
+     *  Convert cups to cups.
+     *  @param quantity
+     *  @return the number of cups.
+     */
     public String cups(double quantity) {
 
         return Double.toString(quantity);
     }
 
+    /**
+     *  Convert cups to tablespoons.
+     *  @param quantity
+     *  @return the number of tablespoons.
+     */
     public String tablespoons(double quantity) {
 
         return Double.toString(quantity*16);
--- design_patterns/src/behavorial/interpreter/Gallons.java
+++ design_patterns/src/behavorial/interpreter/Gallons.java
@@ -5,26 +5,51 @@ package behavorial.interpreter;
  */
 public class Gallons extends Expression{
 
+    /**
+     *  Convert gallons to gallons.
+     *  @param quantity
+     *  @return the number of gallons.
+     */
     public String gallons(double quantity) {
 
         return Double.toString(quantity);
     }
 
+    /**
+     *  Converts gallons to quarts.
+     *  @param quantity
+     *  @return the number of quarts
+     */
     public String quarts(double quantity) {
 
         return Double.toString(quantity * 4);
     }
 
+    /**
+     *  Converts gallons to pints.
+     *  @param quantity
+     *  @return the number of pints.
+     */
     public String pints(double quantity) {
 
         return Double.toString(quantity * 8);
     }
 
+    /**
+     *  Convert gallons to cups.
+     *  @param quantity
+     *  @return the number of cups.
+     */
     public String cups(double quantity) {
 
         return Double.toString(quantity * 16);
     }
 
+    /**
+     *  Convert gallond to tablespoons.
+     *  @param quantity
+     *  @return the number of tablespoons.
+     */
     public String tablespoons(double quantity) {
 
         return Double.toString(quantity * 256);
