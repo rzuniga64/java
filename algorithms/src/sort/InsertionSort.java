021cd910a71993c3a1ec305d2df425192748e111
Raul Zuniga <rzuniga64@gmail.com>
2019-03-12 17:10:21 -0500

    Remove binarySearchForValue method.

--- algorithms/src/sort/InsertionSort.java
+++ algorithms/src/sort/InsertionSort.java
@@ -59,29 +59,6 @@ public class InsertionSort {
         }
     }
 
-    /** The Binary LinearSearch is quicker than the linear search because all the values are sorted. Because everything is
-     * sorted once you get to a number larger than what you are looking for you can stop the search. Also you be able
-     * to start searching from the middle which speeds the search. It also works best when there are no duplicates */
-
-    private void binarySearchForValue(int value){
-
-        int N = theArray.length;
-        int lowIndex = 0;
-        int highIndex = N - 1;
-
-        while(lowIndex <= highIndex){
-
-            int middleIndex = (highIndex + lowIndex) / 2;
-            if(theArray[middleIndex] < value) lowIndex = middleIndex + 1;
-            else if(theArray[middleIndex] > value) highIndex = middleIndex - 1;
-            else {
-                System.out.println("\nFound a Match for " + value + " at Index " + middleIndex);
-                lowIndex = highIndex + 1;
-            }
-            printHorizontalArray(theArray, N, middleIndex, -1);
-        }
-    }
-
     public static void main(String[] args){
 
         generateRandomArray(InsertionSort.theArray, InsertionSort.theArray.length);
